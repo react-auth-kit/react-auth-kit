13c86f5e2302ef6ecbd416dca271bdf6b96f1c2d
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-28 21:05:23 +0530

    feat: Added library files

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
 import license from 'rollup-plugin-license'
+import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import pkg from './package.json'
@@ -11,23 +13,41 @@ const licenseBanner = license({
     }
 })
 
-export default {
-    input: "src/index.tsx",
-    output: [
-        {
-            file: pkg.main,
-            format: "cjs",
-            sourcemap: true
-        },
-        {
-            file: pkg.module,
-            format: "esm",
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        typescript({ useTsconfigDeclarationDir: true }),
-    ]
-};
+export default [
+    {
+        input: "src/index.tsx",
+        output: [
+            {
+                file: pkg.main,
+                format: "cjs",
+            },
+            {
+                file: pkg.module,
+                format: "esm",
+            }
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            typescript({ useTsconfigDeclarationDir: true }),
+        ]
+    },
+    {
+        input: "src/index.tsx",
+        output: [
+            {
+                file: pkg.main.replace('.js', '.umd.js'),
+                format: 'umd',
+                name: 'ReactAuthKit'
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            typescript({ useTsconfigDeclarationDir: true }),
+            terser(),
+            licenseBanner,
+            filesize()
+        ]
+    }
+];
