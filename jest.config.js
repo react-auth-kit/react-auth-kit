82803aa0140eac6e0c3ca2e0627f71333a773d28
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-29 10:02:47 +0530

    feat: jest.config.js added

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    roots: ["<rootDir>/src"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    preset: "ts-jest",
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
