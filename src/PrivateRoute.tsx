b6f0311486a49069146f0c099d61a89948d41748
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-09-08 10:48:09 +0530

    feat: PrivateRoute.tsx now supports render and component props

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,57 +1,60 @@
-import React, { useContext } from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import { AuthContext } from './AuthContext'
+import React, {useContext} from 'react'
+import {Route, Redirect, RouteProps} from 'react-router-dom'
+import {AuthContext} from './AuthContext'
 
-type Props = {
-  Component: React.ElementType
-  loginPath: string
-  rest: any
+interface PrivateRouteProps extends RouteProps {
+    loginPath: string
 }
 
 /**
  * Private Route for Components
  *
  * @remarks
- * This Component is based on {@link react-route-dom.Route}.
+ * This Component is based on {@link https://reactrouter.com/web/api/Route | reactrouter.Route}.
  * So you need to install react-route-dom before use it
  *
- * @param Component - Component to be rendered
- * @param loginPath - Fallback path if authentication fails
- * @param rest - Other props support by Route {@link react-route-dom.Route}
+ * @param props
  */
-const PrivateRoute: React.FunctionComponent<Props> = ({
-  Component,
-  loginPath,
-  ...rest
-}) => {
-  const context = useContext(AuthContext)
+const PrivateRoute: React.FunctionComponent<PrivateRouteProps> = (props) => {
+    const context = useContext(AuthContext)
 
-  const isAuth = () => {
-    if (context?.authState.authToken && context?.authState.expireAt) {
-      if (new Date(context.authState.expireAt) > new Date()) {
-        return true
-      } else {
-        console.log('RAJ :: Token Expired')
-        context.setAuthState({
-          authToken: null,
-          expireAt: null,
-          authState: null
-        })
-        return false
-      }
-    } else {
-      return false
+    const isAuth = () => {
+        if (context?.authState.authToken && context?.authState.expireAt) {
+            if (new Date(context.authState.expireAt) > new Date()) {
+                return true
+            } else {
+                context.setAuthState({
+                    authToken: null,
+                    expireAt: null,
+                    authState: null
+                })
+                return false
+            }
+        } else {
+            return false
+        }
     }
-  }
 
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isAuth() ? <Component props={props} /> : <Redirect to={loginPath} />
-      }
-    />
-  )
+    const {component, loginPath, strict, sensitive, exact, path, location, render} = props
+
+    return (
+        <Route
+            location={location}
+            path={path}
+            exact={exact}
+            sensitive={sensitive}
+            strict={strict}
+            render={(renderProps) =>
+                isAuth() ?
+                    component
+                        ? React.createElement(component, renderProps)
+                        : render
+                        ? render(renderProps)
+                        : null
+                    : <Redirect to={loginPath}/>
+            }
+        />
+    )
 }
 
 export default PrivateRoute
