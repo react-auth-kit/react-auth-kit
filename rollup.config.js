063853e4f1e1beb25e405eb88001a0a5427f5fcb
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-09-12 17:38:10 +0530

    linting: rollup.config.js jest.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,53 @@
-import typescript from 'rollup-plugin-typescript2'
-import license from 'rollup-plugin-license'
-import { terser } from 'rollup-plugin-terser'
-import filesize from 'rollup-plugin-filesize'
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import pkg from './package.json'
+import typescript from 'rollup-plugin-typescript2';
+import license from 'rollup-plugin-license';
+import {terser} from 'rollup-plugin-terser';
+import filesize from 'rollup-plugin-filesize';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
 
 const licenseBanner = license({
-    banner: {
-        content: '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */',
-        commentStyle: 'none'
-    }
-})
+  banner: {
+    content: '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */',
+    commentStyle: 'none',
+  },
+});
 
 export default [
-    {
-        input: "src/index.tsx",
-        output: [
-            {
-                file: pkg.main,
-                format: "cjs",
-            },
-            {
-                file: pkg.module,
-                format: "esm",
-            }
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            typescript({ useTsconfigDeclarationDir: true }),
-        ]
-    },
-    {
-        input: "src/index.tsx",
-        output: [
-            {
-                file: pkg.main.replace('.js', '.umd.js'),
-                format: 'umd',
-                name: 'ReactAuthKit'
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            typescript({ useTsconfigDeclarationDir: true }),
-            terser(),
-            licenseBanner,
-            filesize()
-        ]
-    }
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      typescript({useTsconfigDeclarationDir: true}),
+    ],
+  },
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: pkg.main.replace('.js', '.umd.js'),
+        format: 'umd',
+        name: 'ReactAuthKit',
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      typescript({useTsconfigDeclarationDir: true}),
+      terser(),
+      licenseBanner,
+      filesize(),
+    ],
+  },
 ];
