0c1a728c581271d6acdd8d12120e79a505def6c4
Arkadip <in2arkadipb13@gmail.com>
2020-12-17 22:12:12 +0530

    Done Revert

--- src/hooks/useAuthHeader.ts
+++ src/hooks/useAuthHeader.ts
@@ -1,27 +1,18 @@
-/**
-  *@author Arkadip Bhattacharya <in2arkadipb13@gmail.com>
-  *@fileoverview Authentication Header
-  *@copyright Arkadip Bhattacharya 2020
-  *@license Apache-2.0
-  */
-
 import * as React from 'react';
 import {AuthContext} from '../AuthProvider';
 
 /**
-  *@function
-  *@name useAuthHeader
-  */
-function useAuthHeader(): string {
+ *
+ */
+function useAuthHeader(): () => (string) {
   const c = React.useContext(AuthContext);
-
-  if (c?.authState) {
-    return `${c.authState.authTokenType} ${c.authState.authToken}`;
-  } else {
-    return `Bearer `;
-  }
+  return () => {
+    if (c?.authState) {
+      return `${c.authState.authTokenType} ${c.authState.authToken}`;
+    } else {
+      return `Bearer `;
+    }
+  };
 }
-/**
-  *@exports useAuthHeader
-  */
+
 export default useAuthHeader;
--- src/hooks/useAuthUser.ts
+++ src/hooks/useAuthUser.ts
@@ -1,26 +1,17 @@
-/**
-  *@author Arkadip Bhattacharya <in2arkadipb13@gmail.com>
-  *@fileoverview Auth User
-  *@copyright Arkadip Bhattacharya 2020
-  *@license Apache-2.0
-  */
-
 import * as React from 'react';
 import {AuthContext} from '../AuthProvider';
 
 /**
-  *@function
-  *@name useAuthUser
-  *@description Auth State Hook
-  *@returns - Auth State Function
-  */
-function useAuthUser(): object | null{
+ * Auth State Hook
+ *
+ * @returns - Auth State Function
+ */
+function useAuthUser(): () => object | null{
   const c = React.useContext(AuthContext);
 
-  return c.authState.authState;
+  return () => {
+    return c.authState.authState;
+  };
 }
 
-/**
-  *@exports useAuthUser
-  */
 export default useAuthUser;
