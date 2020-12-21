44e4aa7e11f8fb27f8704040e517a9e8ddd69b57
Arkadip <in2arkadipb13@gmail.com>
2020-12-21 13:55:34 +0530

    refactor: Indented types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,34 @@
 import * as React from 'react';
 
 export interface TokenInterface {
-    authToken: string | null
-    authTokenType: string | null
-    expireAt: Date | null
-    authState: object | null
+  authToken: string | null
+  authTokenType: string | null
+  expireAt: Date | null
+  authState: object | null
 }
 
 export interface signInFunctionParams {
-    token: string,
-    tokenType: string | 'Bearer',
-    expiresIn: number,
-    authState: object
+  token: string
+  tokenType: string | 'Bearer'
+  expiresIn: number
+  authState: object
 }
 
 export interface TokenObjectParamsInterface {
-    authStorageType: 'cookie' | 'localstorage'
-    authStorageName: string,
-    authTimeStorageName: string,
-    stateStorageName: string,
-    cookieDomain?: string
-    cookieSecure?: boolean
+  authStorageType: 'cookie' | 'localstorage'
+  authStorageName: string,
+  authTimeStorageName: string
+  stateStorageName: string
+  refreshTokenName?: string
+  cookieDomain?: string
+  cookieSecure?: boolean
 }
 
 export interface AuthContextInterface {
-    authState: TokenInterface
-    setAuthState: React.Dispatch<React.SetStateAction<TokenInterface>>
+  authState: TokenInterface
+  setAuthState: React.Dispatch<React.SetStateAction<TokenInterface>>
 }
 
 export interface AuthProviderProps extends TokenObjectParamsInterface {
-    children: React.ReactNode
+  children: React.ReactNode
 }
