94d7a992ecb3e47a9d01f3f066cf2e851fce00bc
Arkadip <in2arkadipb13@gmail.com>
2021-06-19 11:23:40 +0530

    Jest Config - Added verbose

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,6 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
+  verbose: true
 };
