18d446f59cb1faf00b5e884dcdcd78dddde025fd
Arkadip <in2arkadipb13@gmail.com>
2021-07-25 17:38:06 +0530

    📄 CHANGELOG updated

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import {Redirect, Route} from 'react-router-dom';
 import AuthContext from './AuthContext';
-import * as H from "history";
-import {RouteChildrenProps, RouteComponentProps} from "react-router";
+import * as H from 'history';
+import {RouteChildrenProps, RouteComponentProps} from 'react-router';
 
 interface PrivateRouteProps {
   loginPath: string
   location?: H.Location;
-  component?: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+  component?: React.ComponentType<RouteComponentProps<any>> |
+    React.ComponentType<any>;
   render?: (props: RouteComponentProps<any>) => React.ReactNode;
-  children?: ((props: RouteChildrenProps<any>) => React.ReactNode) | React.ReactNode;
+  children?: ((props: RouteChildrenProps<any>) => React.ReactNode) |
+    React.ReactNode;
   path?: string | string[];
   exact?: boolean;
   sensitive?: boolean;
