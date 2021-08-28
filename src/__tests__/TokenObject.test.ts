30ef617e779260c55c8e2fdb3874576d9645b595
Arkadip <in2arkadipb13@gmail.com>
2021-08-28 11:02:16 +0530

    test: token object test file added to test the token retrieval and setup - Added 2 testcases    1. to check the isUsingRefreshToken property    2. to check the initial token value

---
+++ src/__tests__/TokenObject.test.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 Arkadip Bhattacharya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import TokenObject from '../TokenObject';
+
+describe('isUsingRefreshToken value is as expected', ()=>{
+  it('isUsingRefreshToken is false', ()=>{
+    const tokenObject = new TokenObject('__', 'cookie',
+        null, window.location.hostname, window.location.protocol === 'https:');
+
+    expect(tokenObject.initialToken().isUsingRefreshToken).toBe(false);
+  });
+
+  it('isUsingRefreshToken is true', ()=>{
+    const tokenObject = new TokenObject('__', 'cookie',
+        '__r', window.location.hostname, window.location.protocol === 'https:');
+
+    expect(tokenObject.initialToken().isUsingRefreshToken).toBe(true);
+  });
+});
+
+describe('Token with Cookie', ()=>{
+  let tokenObject: TokenObject;
+  beforeEach(()=>{
+    tokenObject = new TokenObject('__', 'cookie',
+        null, window.location.hostname, window.location.protocol === 'https:');
+  });
+  it('Initially blank and signed out state', ()=>{
+    const {
+      isSignIn, authToken, authTokenType, refreshToken,
+      refreshTokenExpireAt, expireAt,
+      authState,
+    } = tokenObject.initialToken();
+
+    expect(isSignIn).toBe(false);
+    expect(authState).toBe(null);
+    expect(authToken).toBe(null);
+    expect(authTokenType).toBe(null);
+    expect(refreshToken).toBe(null);
+    expect(refreshTokenExpireAt).toBe(null);
+    expect(expireAt).toBe(null);
+  });
+});
+
+export {};
