4cbb0ba875c14ea9e828a70ed7c1aba42840c0af
Arkadip Bhattacharya <hi@arkadip.dev>
2023-04-10 03:02:56 +0000

    docs(errors): added js-doc

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,22 @@
-export class AuthKitError extends Error{
-    constructor(message: string){
-        super(message)
-    }
+/**
+ * @author Arkadip Bhattacharya <in2arkadipb13@gmail.com>
+ * @fileoverview Sign In functionality <Higher Order Component>
+ * @copyright Arkadip Bhattacharya 2020
+ */
+
+/**
+ * @class
+ * @name AuthKitError
+ * @extends Error
+ *
+ * General Auth kit error class
+ */
+export class AuthKitError extends Error {
+  /**
+     * @constructor
+     * @param message - Error message
+     */
+  constructor(message: string) {
+    super(message);
+  }
 }
