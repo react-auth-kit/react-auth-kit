177eb9bce144f792b04ae606e4eb98e2df7bcf14
Arkadip Bhattacharya <hi@arkadip.dev>
2023-10-06 07:45:19 +0000

    test(example): updated the app to create root

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AuthProvider} from 'react-auth-kit'
+import AuthProvider from 'react-auth-kit/AuthProvider'
 import RoutesComponent from './Routes';
 
 function App() {
