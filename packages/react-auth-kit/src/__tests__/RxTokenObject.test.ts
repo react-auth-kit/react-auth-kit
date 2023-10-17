fd8d4824451cb1dfc5878a765991514f7c12cfee
Arkadip Bhattacharya <hi@arkadip.dev>
2023-10-17 16:15:23 +0000

    test(refresh): tampered auth proper jwt

--- packages/react-auth-kit/src/__tests__/RxTokenObject.test.ts
+++ packages/react-auth-kit/src/__tests__/RxTokenObject.test.ts
@@ -242,185 +242,184 @@ describe('Initial Value [With Refresh Token]', () => {
     expect(tokenObject.value).toMatchObject({"auth": null, "isSignIn": false, "isUsingRefreshToken": true, "refresh": null, "userState": null})
   });
 
-  // describe('Existing Cookies are there', () => {
+  describe('Existing Cookies are there', () => {
 
-  //   beforeEach(()=>{
-  //     Cookies.set('___type', 'Bearer');
-  //     Cookies.set('___state', '{}');
-  //   })
+    beforeEach(()=>{
+      Cookies.set('___type', 'Bearer');
+      Cookies.set('___state', '{}');
+    })
 
-  //   afterEach(()=>{
-  //     Cookies.remove('__');
-  //     Cookies.remove('___type');
-  //     Cookies.remove('___state');
-  //     Cookies.remove('__re');
+    afterEach(()=>{
+      Cookies.remove('__');
+      Cookies.remove('___type');
+      Cookies.remove('___state');
+      Cookies.remove('__re');
+    });
 
-  //   });
+    it('Existing Cookies are there', () => {
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjA1MTk1fQ.E0EVT_4KVJHPEnC8XmukxiRRcAIo31U9wWW99RVQumA'
+      Cookies.set('__', token);
 
-  //   it('Existing Cookies are there', () => {
-  //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjA1MTk1fQ.E0EVT_4KVJHPEnC8XmukxiRRcAIo31U9wWW99RVQumA'
-  //     Cookies.set('__', token);
-
-  //     const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjIwODYzfQ.pXpDIqK20WJgkzMLbR7yjL4VD-NBMYsOVptOGR7Wf2E'
-  //     Cookies.set('__re', refreshToken);
+      const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjIwODYzfQ.pXpDIqK20WJgkzMLbR7yjL4VD-NBMYsOVptOGR7Wf2E'
+      Cookies.set('__re', refreshToken);
 
 
-  //     const tokenObject = new TokenObject<object>(
-  //       '__',
-  //       'cookie',
-  //       '__re',
-  //       window.location.hostname,
-  //       window.location.protocol === 'https:',
-  //     );
+      const tokenObject = new TokenObject<object>(
+        '__',
+        'cookie',
+        '__re',
+        window.location.hostname,
+        window.location.protocol === 'https:',
+      );
       
-  //     expect(tokenObject.value).toMatchObject(
-  //       {
-  //         "auth": {
-  //           'token': token,
-  //           'type': 'Bearer',
-  //           'expiresAt': new Date(8008605195*1000),
-  //         }, 
-  //         "isSignIn": true, 
-  //         "isUsingRefreshToken": true, 
-  //         "refresh": {
-  //           'token': refreshToken,
-  //           'expiresAt': new Date(8008620863 * 1000)
-  //         }, 
-  //         "userState": {}
-  //       }
-  //     )
-  //   });
+      expect(tokenObject.value).toMatchObject(
+        {
+          "auth": {
+            'token': token,
+            'type': 'Bearer',
+            'expiresAt': new Date(8008605195*1000),
+          }, 
+          "isSignIn": true, 
+          "isUsingRefreshToken": true, 
+          "refresh": {
+            'token': refreshToken,
+            'expiresAt': new Date(8008620863 * 1000)
+          }, 
+          "userState": {}
+        }
+      )
+    });
 
-  //   it('Existing Auth Cookie is not a proper JWT but Refresh Token is not proper JWT', () => {
-  //     const token = 'tampered_'
-  //     Cookies.set('__', token);
+    it('Existing Auth Cookie is not a proper JWT but Refresh Token is not proper JWT', () => {
+      const token = 'tampered_'
+      Cookies.set('__', token);
 
-  //     const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjIwODYzfQ.pXpDIqK20WJgkzMLbR7yjL4VD-NBMYsOVptOGR7Wf2E'
-  //     Cookies.set('__re', refreshToken);
+      const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjIwODYzfQ.pXpDIqK20WJgkzMLbR7yjL4VD-NBMYsOVptOGR7Wf2E'
+      Cookies.set('__re', refreshToken);
 
-  //     expect(Cookies.get('__')).toBe(token)
-  //     expect(Cookies.get('___type')).toBe('Bearer')
-  //     expect(Cookies.get('___state')).toBe('{}')
-  //     expect(Cookies.get('__re')).toBe(refreshToken)
+      expect(Cookies.get('__')).toBe(token)
+      expect(Cookies.get('___type')).toBe('Bearer')
+      expect(Cookies.get('___state')).toBe('{}')
+      expect(Cookies.get('__re')).toBe(refreshToken)
 
 
-  //     const tokenObject = new TokenObject<object>(
-  //       '__',
-  //       'cookie',
-  //       null,
-  //       window.location.hostname,
-  //       window.location.protocol === 'https:',
-  //     );
+      const tokenObject = new TokenObject<object>(
+        '__',
+        'cookie',
+        '__re',
+        window.location.hostname,
+        window.location.protocol === 'https:',
+      );
       
-  //     expect(tokenObject.value).toMatchObject(
-  //       {
-  //         "auth": null, 
-  //         "isSignIn": false, 
-  //         "isUsingRefreshToken": true, 
-  //         "refresh": {
-  //           'token': refreshToken,
-  //           'expiresAt': new Date(8008620863 * 1000)
-  //         }, 
-  //         "userState": null
-  //       }
-  //     )
+      expect(tokenObject.value).toMatchObject(
+        {
+          "auth": null, 
+          "isSignIn": false, 
+          "isUsingRefreshToken": true, 
+          "refresh": {
+            'token': refreshToken,
+            'expiresAt': new Date(8008620863 * 1000)
+          }, 
+          "userState": null
+        }
+      )
 
-  //     expect(Cookies.get('__')).toBeUndefined()
-  //     expect(Cookies.get('___type')).toBeUndefined()
-  //     expect(Cookies.get('___state')).toBeUndefined()
-  //     expect(Cookies.get('__re')).toBe(refreshToken)
-  //   });
+      expect(Cookies.get('__')).toBeUndefined()
+      expect(Cookies.get('___type')).toBeUndefined()
+      expect(Cookies.get('___state')).toBeUndefined()
+      expect(Cookies.get('__re')).toBe(refreshToken)
+    });
 
-  //   it('Existing Auth Cookie and Refresh Cookies are not a proper JWT', () => {
-  //     const token = 'tampered_'
-  //     Cookies.set('__', token);
+    // it('Existing Auth Cookie and Refresh Cookies are not a proper JWT', () => {
+    //   const token = 'tampered_'
+    //   Cookies.set('__', token);
 
-  //     const refreshToken = 'tempered__'
-  //     Cookies.set('__re', refreshToken);
+    //   const refreshToken = 'tempered__'
+    //   Cookies.set('__re', refreshToken);
 
-  //     expect(Cookies.get('__')).toBe(token)
-  //     expect(Cookies.get('___type')).toBe('Bearer')
-  //     expect(Cookies.get('___state')).toBe('{}')
+    //   expect(Cookies.get('__')).toBe(token)
+    //   expect(Cookies.get('___type')).toBe('Bearer')
+    //   expect(Cookies.get('___state')).toBe('{}')
 
-  //     const tokenObject = new TokenObject<object>(
-  //       '__',
-  //       'cookie',
-  //       null,
-  //       window.location.hostname,
-  //       window.location.protocol === 'https:',
-  //     );
+    //   const tokenObject = new TokenObject<object>(
+    //     '__',
+    //     'cookie',
+    //     null,
+    //     window.location.hostname,
+    //     window.location.protocol === 'https:',
+    //   );
       
-  //     expect(tokenObject.value).toMatchObject(
-  //       {
-  //         "auth": null, 
-  //         "isSignIn": false, 
-  //         "isUsingRefreshToken": false, 
-  //         "refresh": null, 
-  //         "userState": null
-  //       }
-  //     )
+    //   expect(tokenObject.value).toMatchObject(
+    //     {
+    //       "auth": null, 
+    //       "isSignIn": false, 
+    //       "isUsingRefreshToken": false, 
+    //       "refresh": null, 
+    //       "userState": null
+    //     }
+    //   )
 
-  //     expect(Cookies.get('__')).toBeUndefined()
-  //     expect(Cookies.get('___type')).toBeUndefined()
-  //     expect(Cookies.get('___state')).toBeUndefined()
-  //     expect(Cookies.get('__re')).toBe(refreshToken)
-  //   });
+    //   expect(Cookies.get('__')).toBeUndefined()
+    //   expect(Cookies.get('___type')).toBeUndefined()
+    //   expect(Cookies.get('___state')).toBeUndefined()
+    //   expect(Cookies.get('__re')).toBe(refreshToken)
+    // });
 
-  //   it('Existing Auth Cookie was already expired but Refresh Cookie is not expired', () => {
-  //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo5NzE0OTc5OTV9.XJbNAE-aRz7tO7tSHiUlMGGuUrAELPPkNITKVlNZ8DA'
-  //     Cookies.set('__', token);
+    // it('Existing Auth Cookie was already expired but Refresh Cookie is not expired', () => {
+    //   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo5NzE0OTc5OTV9.XJbNAE-aRz7tO7tSHiUlMGGuUrAELPPkNITKVlNZ8DA'
+    //   Cookies.set('__', token);
 
-  //     const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjIwODYzfQ.pXpDIqK20WJgkzMLbR7yjL4VD-NBMYsOVptOGR7Wf2E'
-  //     Cookies.set('__re', refreshToken);
+    //   const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo4MDA4NjIwODYzfQ.pXpDIqK20WJgkzMLbR7yjL4VD-NBMYsOVptOGR7Wf2E'
+    //   Cookies.set('__re', refreshToken);
 
-  //     const tokenObject = new TokenObject<object>(
-  //       '__',
-  //       'cookie',
-  //       '__re',
-  //       window.location.hostname,
-  //       window.location.protocol === 'https:',
-  //     );
+    //   const tokenObject = new TokenObject<object>(
+    //     '__',
+    //     'cookie',
+    //     '__re',
+    //     window.location.hostname,
+    //     window.location.protocol === 'https:',
+    //   );
       
-  //     expect(tokenObject.value).toMatchObject(
-  //       {
-  //         "auth": null, 
-  //         "isSignIn": false, 
-  //         "isUsingRefreshToken": false, 
-  //         "refresh": {
-  //           'token': refreshToken,
-  //           'expiresAt': new Date(8008620863 * 1000)
-  //         }, 
-  //         "userState": {}
-  //       }
-  //     )
-  //   });
+    //   expect(tokenObject.value).toMatchObject(
+    //     {
+    //       "auth": null, 
+    //       "isSignIn": false, 
+    //       "isUsingRefreshToken": false, 
+    //       "refresh": {
+    //         'token': refreshToken,
+    //         'expiresAt': new Date(8008620863 * 1000)
+    //       }, 
+    //       "userState": {}
+    //     }
+    //   )
+    // });
 
-  //   it('Existing Auth Cookie and Refresh Cookie were already expired', () => {
-  //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo5NzE0OTc5OTV9.XJbNAE-aRz7tO7tSHiUlMGGuUrAELPPkNITKVlNZ8DA'
-  //     Cookies.set('__', token);
+    // it('Existing Auth Cookie and Refresh Cookie were already expired', () => {
+    //   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo5NzE0OTc5OTV9.XJbNAE-aRz7tO7tSHiUlMGGuUrAELPPkNITKVlNZ8DA'
+    //   Cookies.set('__', token);
 
-  //     const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo5NzE0OTc5OTV9.XJbNAE-aRz7tO7tSHiUlMGGuUrAELPPkNITKVlNZ8DA'
-  //     Cookies.set('__re', refreshToken);
+    //   const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo5NzE0OTc5OTV9.XJbNAE-aRz7tO7tSHiUlMGGuUrAELPPkNITKVlNZ8DA'
+    //   Cookies.set('__re', refreshToken);
 
-  //     const tokenObject = new TokenObject<object>(
-  //       '__',
-  //       'cookie',
-  //       '__re',
-  //       window.location.hostname,
-  //       window.location.protocol === 'https:',
-  //     );
+    //   const tokenObject = new TokenObject<object>(
+    //     '__',
+    //     'cookie',
+    //     '__re',
+    //     window.location.hostname,
+    //     window.location.protocol === 'https:',
+    //   );
       
-  //     expect(tokenObject.value).toMatchObject(
-  //       {
-  //         "auth": null, 
-  //         "isSignIn": false, 
-  //         "isUsingRefreshToken": false, 
-  //         "refresh": null,
-  //         "userState": {}
-  //       }
-  //     )
-  //   });
-  // });
+    //   expect(tokenObject.value).toMatchObject(
+    //     {
+    //       "auth": null, 
+    //       "isSignIn": false, 
+    //       "isUsingRefreshToken": false, 
+    //       "refresh": null,
+    //       "userState": {}
+    //     }
+    //   )
+    // });
+  });
 
 
 
