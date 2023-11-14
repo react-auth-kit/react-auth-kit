053eef1f57c65d720ae93f3d79e0420d48b913f0
Arkadip Bhattacharya <hi@arkadip.dev>
2023-11-14 16:51:54 +0000

    refactor(use auth header): added ts doc type param

--- packages/react-auth-kit/src/hooks/useAuthHeader.ts
+++ packages/react-auth-kit/src/hooks/useAuthHeader.ts
@@ -1,38 +1,53 @@
-/**
- *
- * @author Arkadip Bhattacharya <hi@arkadip.dev>
- * @fileoverview Authentication header <hook>
- * @copyright Arkadip Bhattacharya 2020
- *
- */
-
-import * as React from 'react';
+import {useContext} from 'react';
 import AuthContext from '../AuthContext';
 import {AuthError} from '../errors';
 import {isAuthenticated} from '../utils/utils';
 
 /**
+ * Auth Header React Hook
  *
+ * Call the hook,
+ * to get the auth header in the format `type token` for network request
+ *
+ * @example
+ * Here is a simple example
+ * ```jsx
+ * import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader'
+ *
+ * const Component = () => {
+ *  const authHeader = useAuthHeader();
+ *  const headers = {
+ *    'Authorization': authHeader
+ *  }
+ *  // use the headers in the network request
+ *  ...
+ * }
+ * ```
+ *
+ * @throws AuthError
+ * Thrown if the Hook is used outside the Provider Scope.
+ *
+ * @returns If the user is authenticated,
+ * then `'auth.type auth.token'` is returned.
+ * If the user is not authenticated, then `null` is ruturned.
  */
-function useAuthHeader(): () => (string) {
-  const c = React.useContext(AuthContext);
+function useAuthHeader(): string | null {
+  const c = useContext(AuthContext);
   if (c === null) {
     throw new
     AuthError(
         'Auth Provider is missing. ' +
-      'Make sure, you are using this hook inside the auth provider.',
+        'Make sure, you are using this hook inside the auth provider.',
     );
   }
 
+  const {value} = c;
 
-  return () => {
-    const obj = c.value;
-    if (!!obj.auth && isAuthenticated(obj)) {
-      return `${obj.auth.type} ${obj.auth.token}`;
-    } else {
-      return ``;
-    }
-  };
+  if (!!value.auth && isAuthenticated(value)) {
+    return `${value.auth.type} ${value.auth.token}`;
+  } else {
+    return null;
+  }
 }
 
 export default useAuthHeader;
