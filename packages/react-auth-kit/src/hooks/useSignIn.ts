ca0ed361fa0af757b57b59fb8ac0f50f9d9ea4b7
Arkadip Bhattacharya <hi@arkadip.dev>
2023-11-14 07:54:31 +0000

    refactor(use sign in): added ts doc

--- packages/react-auth-kit/src/hooks/useSignIn.ts
+++ packages/react-auth-kit/src/hooks/useSignIn.ts
@@ -1,30 +1,72 @@
-/**
- *
- * @author Arkadip Bhattacharya <hi@arkadip.dev>
- * @fileoverview Sign In functionality <hook>
- * @copyright Arkadip Bhattacharya 2020
- * @license MIT
- *
- */
-
-import * as React from 'react';
+import {useContext} from 'react';
 import AuthContext from '../AuthContext';
 import {signInFunctionParams} from '../types';
 import {doSignIn} from '../utils/reducers';
 import {AuthError} from '../errors';
 
 /**
- *@function
- *@name useSignIn
- *@description Authentication SignIn Hook
- *@returns - Sign In function
+ * Sign In React Hook
+ *
+ * Call the hook,
+ * when you want to sign In and authenticate the user
+ *
+ * This will authenticate the user by writing the uer state into the mamory
+ * Also, this will call the rx engine to store the auth into into the storage
+ *
+ * @example
+ * Here's a an example without refresh token:
+ * ```js
+ * import useSignIn from 'react-auth-kit/hooks/useSignIn'
+ *
+ * const LoginComponent = () => {
+ *  const signIn = useSignIn()
+ *  signIn({
+ *    auth: {
+ *      token: '<jwt token>'
+ *    },
+ *    userState: {name: 'React User', uid: 123456}
+ *  })
+ * }
+ * ```
+ *
+ * Here's a an example with refresh token:
+ * ```js
+ * import useSignIn from 'react-auth-kit/hooks/useSignIn'
+ *
+ * const LoginComponent = () => {
+ *  const signIn = useSignIn()
+ *  signIn({
+ *    auth: {
+ *      token: '<jwt token>'
+ *    },
+ *    userState: {name: 'React User', uid: 123456},
+ *    refresh: <refresh jwt token>
+ *  })
+ * }
+ * ```
+ *
+ * @remarks
+ * If you are using refresh token, make sure you add that in the parameter,
+ * else it throws AuthError
+ *
+ * If you are not using refresh token, make sure you don't include
+ * that in the parameter, else it throws AuthError.
+ *
+ * @throws AuthError
+ * Thrown if the Hook is used outside the Provider Scope.
+ *
+ * Thrown if refresh token is added, in spite not used.
+ *
+ * Thrown if refresh token is not added, is spite used.
+ *
+ * @returns React Hook with SignIn Functionility
  */
 function useSignIn<T>(): (signInConfig: signInFunctionParams<T>) => boolean {
-  const context = React.useContext(AuthContext);
+  const context = useContext(AuthContext);
   if (context === null) {
     throw new
     AuthError('Auth Provider is missing. ' +
-      'Please add the AuthProvider before Router');
+      'Make sure, you are using this hook inside the auth provider');
   }
   return (signInConfig: signInFunctionParams<T>): boolean => {
     if (context.value.isUsingRefreshToken) {
@@ -37,17 +79,22 @@ function useSignIn<T>(): (signInConfig: signInFunctionParams<T>) => boolean {
       } else {
         // refresh token params are not provided
         // throw an error
-        throw new AuthError('Make sure you given "refreshToken" and  ' +
-          '"refreshTokenExpireIn" parameter');
+        throw new AuthError(
+            'This appication is using refresh token feature.'+
+          ' So please include `refresh` param in the parameters',
+        );
       }
     } else {
       // Not using refresh token
       if (signInConfig.refresh) {
         // params are not expected but provided
         // throw an error
-        throw new Error('The app doesn\'t implement \'refreshToken\' ' +
-          'feature.\nSo you have to implement refresh token feature ' +
-          'from \'AuthProvider\' before using it.');
+        throw new Error(
+            'This appication is not using refresh token feature.'+
+          ' So please remove the `refresh` param in the parameters.'+
+          ' In Case you want to use refresh token feature,'+
+          ' make sure you added that while creating the store.',
+        );
       } else {
         // sign in without the refresh token
         context.set(doSignIn(signInConfig));
@@ -57,7 +104,4 @@ function useSignIn<T>(): (signInConfig: signInFunctionParams<T>) => boolean {
   };
 }
 
-/**
- *@exports useSignIn
- */
 export default useSignIn;
