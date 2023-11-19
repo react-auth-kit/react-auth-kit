8b7511eec204b774c6c55fc76a6f1bd6d50705b3
Arkadip Bhattacharya <hi@arkadip.dev>
2023-11-19 12:58:16 +0000

    feat: docs

--- examples/create-react-app/src/Routes.js
+++ examples/create-react-app/src/Routes.js
@@ -8,19 +8,19 @@ import Login from './components/Login'
 import SecureComponent from './components/SecureComponent'
 
 const RoutesComponent = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={'/'} element={<Home/>}/>
-                <Route path={'/login' } element={<Login/>}/>
-                <Route path={'/secure'} element={
-                  <RequireAuth loginPath={'/login'}>
-                    <SecureComponent/>
-                  </RequireAuth>
-                }/>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Home/>}/>
+          <Route path={'/login' } element={<Login/>}/>
+          <Route path={'/secure'} element={
+            <RequireAuth loginPath={'/login'}>
+              <SecureComponent/>
+            </RequireAuth>
+          }/>
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default RoutesComponent
