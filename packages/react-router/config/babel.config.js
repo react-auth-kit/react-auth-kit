09aa7810a99a358132b59e7ab5558387bf9273d5
Arkadip Bhattacharya <hi@arkadip.dev>
2023-11-20 16:38:46 +0000

    feat(react-router): added all the build test lint config

---
+++ packages/react-router/config/babel.config.js
@@ -0,0 +1,43 @@
+// @ts-check
+// We aim to have the same support as Next.js
+// https://nextjs.org/docs/getting-started#system-requirements
+// https://nextjs.org/docs/basic-features/supported-browsers-features
+
+/** @type {import("@babel/core").ConfigFunction} */
+module.exports = (api) => {
+    const isTest = api.env("test")
+    if (isTest) {
+      return {
+        presets: [
+          "@babel/preset-env",
+          ["@babel/preset-react", { runtime: "automatic" }],
+          ["@babel/preset-typescript", { isTSX: true, allExtensions: true }],
+        ],
+      }
+    }
+    return {
+      presets: [
+        ["@babel/preset-env", { targets: { node: 12 } }],
+        "@babel/preset-typescript",
+      ],
+      plugins: [
+        "@babel/plugin-transform-optional-catch-binding",
+        "@babel/plugin-transform-runtime",
+      ],
+      ignore: [
+        "../src/**/__tests__/**",
+      ],
+      comments: false,
+      overrides: [
+        {
+          test: [
+            // TODO
+          ],
+          presets: [
+            ["@babel/preset-env", { targets: { ie: 11 } }],
+            ["@babel/preset-react", { runtime: "automatic" }],
+          ],
+        }
+      ]
+    }
+  }
