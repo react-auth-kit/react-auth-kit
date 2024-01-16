3be90b39e62e5c543e93d7789084a56d99822d5c
Arkadip Bhattacharya <hi@arkadip.dev>
2024-01-16 13:44:39 +0000

    docs: updated the layout with proper hook

--- examples/create-next-ts-route/src/app/secure/layout.tsx
+++ examples/create-next-ts-route/src/app/secure/layout.tsx
@@ -7,15 +7,5 @@ export default function DashboardLayout({
   }: {
     children: React.ReactNode
   }) {
-    const isAuth = useAuth({ fallbackPath: '/login'});
-    console.log("Calling Layout");
-    console.log(isAuth);
-    
-    
-    if(isAuth){
-        return children;
-    }
-    else {
-        return <></>;
-    }
+    return useAuth({ fallbackPath: '/login'}) && children;
   }
