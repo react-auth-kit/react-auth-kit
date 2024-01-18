e133a9e73451efce95febe26bf945cd471e7b7b2
Arkadip Bhattacharya <hi@arkadip.dev>
2024-01-18 05:53:08 +0000

    docs: added tsdocs

--- packages/next/src/index.tsx
+++ packages/next/src/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import * as React from 'react'
-import { useRouter } from "next/navigation";
+import * as React from 'react';
+import {useRouter} from 'next/navigation';
 
-import { useReactAuthKitContext } from 'react-auth-kit/AuthContext';
+import {useReactAuthKitContext} from 'react-auth-kit/AuthContext';
 import {isAuthenticated} from 'react-auth-kit/utils/utils';
 import {doSignOut} from 'react-auth-kit/utils/reducers';
 
@@ -22,25 +22,45 @@ interface NextAuthProps {
    children: React.ReactNode
  }
 
-export function NextAuth({ fallbackPath, children }: NextAuthProps) {
-   const context = useReactAuthKitContext();
-   const [login, setLogIn] = React.useState(false);
-   
-   const { push } = useRouter();
-  
-   React.useEffect(() => {
-      if (!isAuthenticated(context.value)) {
-         // Redirect them to the /login page, but save the current location they
-         // were trying to go to when they were redirected. This allows us to
-         // send them along to that page after they login, which is a nicer
-         // user experience than dropping them off on the home page.
-         context.set(doSignOut());
-         push(fallbackPath);
-      }
-      else {
-         setLogIn(true);
-      }
-   }, []);
-   
-   return login && children;
+/**
+  * 
+  * Next Js Page Wrapper component.
+  * 
+  * @param props - The Properties of the component 
+  * @returns - Compoent with React Auth Kit integrated
+  * 
+  * @example
+  * ```jsx
+  * // layout.jsx
+  * 
+  * export default function Layout({
+  *   children,
+  * }: {
+  *   children: React.ReactNode
+  * }) {
+  *   return <NextAuth fallbackPath={'/login'}>{children}</NextAuth>
+  * }
+  * ```
+  * 
+  */
+export function NextAuth({fallbackPath, children}: NextAuthProps) {
+  const context = useReactAuthKitContext();
+  const [login, setLogIn] = React.useState(false);
+
+  const {push} = useRouter();
+
+  React.useEffect(() => {
+    if (!isAuthenticated(context.value)) {
+      // Redirect them to the /login page, but save the current location they
+      // were trying to go to when they were redirected. This allows us to
+      // send them along to that page after they login, which is a nicer
+      // user experience than dropping them off on the home page.
+      context.set(doSignOut());
+      push(fallbackPath);
+    } else {
+      setLogIn(true);
+    }
+  }, []);
+
+  return login && children;
 }
--- packages/next/src/useNextAuth.tsx
+++ packages/next/src/useNextAuth.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import * as React from 'react'
-import { useRouter } from "next/navigation";
+import * as React from 'react';
+import {useRouter} from 'next/navigation';
 
 import {useReactAuthKitContext} from 'react-auth-kit/AuthContext';
 import {isAuthenticated} from 'react-auth-kit/utils/utils';
@@ -12,33 +12,56 @@ import {doSignOut} from 'react-auth-kit/utils/reducers';
  * Component Props for Auth Outlet
  */
 interface NextAuthProps {
-   /**
-    * Path to redirect if the user is not authenticated
-    *
-    * @example
-    * `/login`
-    */
-   fallbackPath: string
- }
+  /**
+  * Path to redirect if the user is not authenticated
+  *
+  * @example
+  * `/login`
+  */
+  fallbackPath: string
+}
 
-export function useNextAuth ({fallbackPath}:NextAuthProps):boolean {
-   const context = useReactAuthKitContext()
-   const [login, setLogIn] = React.useState(false);
 
-   const { push } = useRouter();
-  
-   React.useEffect(() => {
-      if (!isAuthenticated(context.value)) {
-         // Redirect them to the /login page, but save the current location they
-         // were trying to go to when they were redirected. This allows us to
-         // send them along to that page after they login, which is a nicer
-         // user experience than dropping them off on the home page.
-         context.set(doSignOut());
-         push(fallbackPath);
-      }
-      else {
-         setLogIn(true);
-      }
-   }, []);
-   return login;
-};
+/**
+ * React hook to implement the NextAuth feature
+ * @param arg - Params of the hook 
+ * @returns - Authentication Ready state
+ * 
+ * @remarks
+ * Usage of this hook will make the page CSR, any SSR will not work in
+ * private routes, as the private data in stored on the client side,
+ * only CSR is available at this moment.
+ * 
+ * @example
+ * ```jsx
+ * 'use client'
+ * import { useAuth } from "@auth-kit/next/useAuth"
+ * export default function Layout({
+ *  children,
+ * }: {
+ *  children: React.ReactNode
+ * }) {
+ *  return useAuth({ fallbackPath: '/login'}) && children;
+ * }
+ * ```
+ */
+export default function useNextAuth({fallbackPath}:NextAuthProps):boolean {
+  const context = useReactAuthKitContext();
+  const [login, setLogIn] = React.useState(false);
+
+  const {push} = useRouter();
+
+  React.useEffect(() => {
+    if (!isAuthenticated(context.value)) {
+      // Redirect them to the /login page, but save the current location they
+      // were trying to go to when they were redirected. This allows us to
+      // send them along to that page after they login, which is a nicer
+      // user experience than dropping them off on the home page.
+      context.set(doSignOut());
+      push(fallbackPath);
+    } else {
+      setLogIn(true);
+    }
+  }, []);
+  return login;
+}
