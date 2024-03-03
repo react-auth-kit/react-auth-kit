ca9fbd365edbcc51d1bcfd9785fde5cdc98ec249
Arkadip Bhattacharya <hi@arkadip.dev>
2024-03-03 05:42:33 +0000

    test: createStore coverage 100

--- packages/react-auth-kit/src/__tests__/createStore.test.ts
+++ packages/react-auth-kit/src/__tests__/createStore.test.ts
@@ -1,4 +1,5 @@
 import TokenObject from "../RxTokenObject";
+import createRefresh, { createRefreshParamInterface } from "../createRefresh";
 import createStore from "../createStore";
 import { AuthError } from "../errors";
 
@@ -8,7 +9,7 @@ describe('Store without refreshtoken', ()=>{
             authName: '__auth',
             authType: 'localstorage'
         });
-        const tokenObject = new TokenObject('__auth', "localstorage", null);
+        const tokenObject = new TokenObject('__auth', "localstorage", null, false, );
         expect(store.refresh).toBeUndefined();
         expect(store.tokenObject.value).toEqual(tokenObject.value);
         
@@ -21,7 +22,62 @@ describe('Store without refreshtoken', ()=>{
             cookieDomain: window.location.hostname,
             cookieSecure: false
         });
-        const tokenObject = new TokenObject('__auth', "cookie", null, window.location.hostname, false);
+        const tokenObject = new TokenObject('__auth', "cookie", null, false, window.location.hostname, false);
+        expect(store).not.toThrow(AuthError);
+        expect(store().refresh).toBeUndefined();
+        expect(store().tokenObject.value).toEqual(tokenObject.value);
+    });
+});
+
+describe('Store with refreshtoken', ()=>{
+    let refresh: createRefreshParamInterface<object>;
+    beforeAll(()=>{
+        refresh = createRefresh({
+            interval: 10,
+            refreshApiCallback: async (param) => {
+                param.authToken
+                param.authUserState
+                param.refreshToken
+        
+                return {
+                    isSuccess: true,
+                    newAuthToken: param.authToken || "Hello"
+                }
+            }
+        });
+    })
+
+    it('Store creation local store', ()=> {
+        const store = createStore({
+            authName: '__auth',
+            authType: 'localstorage',
+            refresh: refresh,
+            debug: true
+        });
+        const tokenObject = new TokenObject('__auth', "localstorage", '__auth_state', false);
+        expect(store.refresh).toBeUndefined();
+        expect(store.tokenObject.value).toEqual(tokenObject.value);
+        
+    });
+
+    it('Store creation cookie', ()=> {
+        const store = () => createStore({
+            authName: '__auth',
+            authType: 'cookie',
+            cookieDomain: window.location.hostname,
+            cookieSecure: false,
+            refresh: refresh,
+            debug: true
+        });
+        const tokenObject = new TokenObject(
+            '__auth',
+            "cookie",
+            '__auth_state',
+            false,
+            window.location.hostname,
+            false
+        );
+
         expect(store).not.toThrow(AuthError);
         expect(store().refresh).toBeUndefined();
         expect(store().tokenObject.value).toEqual(tokenObject.value);
