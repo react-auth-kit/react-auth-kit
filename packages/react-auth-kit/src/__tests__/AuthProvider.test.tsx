4494806ca342146ce87ed7d54209e42a634c815d
Arkadip Bhattacharya <hi@arkadip.dev>
2024-03-09 15:50:40 +0000

    test: added auth provider

---
+++ packages/react-auth-kit/src/__tests__/AuthProvider.test.tsx
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/react';
+
+import TokenObject from '../RxTokenObject';
+import AuthProvider from './../AuthProvider';
+// import createRefresh from '../createRefresh';
+// import Cookies from 'js-cookie';
+
+jest.useFakeTimers();
+
+describe('AuthProvider', () => {
+  it('Renders Successfully', () => {
+    const tokenObject = new TokenObject<object>(
+      '__',
+      'cookie',
+      null,
+      false,
+      window.location.hostname,
+      window.location.protocol === 'https:',
+    );
+
+    render(
+      <AuthProvider store={tokenObject as any}>
+        <div id="test">
+          Hello
+        </div>
+      </AuthProvider>
+    );
+    const data = document.querySelector('#test');
+    expect(data?.innerHTML).toEqual(`Hello`);
+  });
+  
+
+});
