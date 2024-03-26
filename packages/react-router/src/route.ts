c6e0b445a12ad94d239af6a415b1718018ca4fe5
Arkadip Bhattacharya <hi@arkadip.dev>
2024-03-26 12:30:55 +0000

    refactor: implemented the ReactRouterPlugin as object

--- packages/react-router/src/route.ts
+++ packages/react-router/src/route.ts
@@ -2,14 +2,14 @@ import Router from "react-auth-kit/route";
 
 import { useNavigate as useReactRouteNavigate, redirect } from "react-router";
 
-class ReactRouterPlugin implements Router{
-    navigate({ to }: { to: string; }): void {
+const ReactRouterPlugin: Router = {
+    navigate: function ({ to }: { to: string; }): void {
         redirect(to)
-    }
-    useNavigate(): ({ to }: { to: string; }) => void {
+    },
+    useNavigate: function (): ({ to }: { to: string; }) => void {
         const navigate = useReactRouteNavigate();
         return ({to}: { to: string; })=> navigate(to)
-    }   
+    }
 }
 
 export default ReactRouterPlugin;
