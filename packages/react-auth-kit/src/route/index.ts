edfdd2f54a3524a8f8745939d6749ee76c7e0bdc
Arkadip Bhattacharya <hi@arkadip.dev>
2024-03-26 12:36:20 +0000

    feat: added tsdocs

--- packages/react-auth-kit/src/route/index.ts
+++ packages/react-auth-kit/src/route/index.ts
@@ -1,5 +1,18 @@
+/**
+ * Router Plugin for React Auth Kit
+ * It's used to navigate between pages when needed by react-auth-kit
+ * 
+ */
 interface Router {
+    /**
+     * Navigate function
+     * @param param0 - object with {to} -> navigate to pagaram
+     */
     navigate: ({to}: {to: string}) => void
+    /**
+     * Hook to navigate
+     * @returns Hook function
+     */
     useNavigate: () => ({to}: {to: string}) => void
 }
 
