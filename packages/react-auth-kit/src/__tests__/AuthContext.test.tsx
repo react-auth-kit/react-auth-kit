ead2a56dd9d659ffe6380020d07f7912beaad123
Arkadip Bhattacharya <hi@arkadip.dev>
2024-04-03 09:04:03 +0000

    test(authcontext): new test cases added

--- packages/react-auth-kit/src/__tests__/AuthContext.test.tsx
+++ packages/react-auth-kit/src/__tests__/AuthContext.test.tsx
@@ -1,51 +1,203 @@
+
+
 import AuthKitContext from '../AuthContext';
-import {useReactAuthKit} from '../AuthContext';
+import {
+  useReactAuthKit,
+  useReactAuthKitConfig,
+  useReactAuthKitRouter
+} from '../AuthContext';
 import {render} from '@testing-library/react';
 import Cookies from 'js-cookie';
 import TokenObject from '../RxTokenObject';
 import {AuthError} from '../errors';
+import Router from '../route';
 
 test('All Expected Exports are there', ()=>{
   expect(AuthKitContext).toBeTruthy();
   expect(useReactAuthKit).toBeTruthy();
+  expect(useReactAuthKitConfig).toBeTruthy();
+  expect(useReactAuthKitRouter).toBeTruthy();
 });
 
-test('Testing Conext Workflow', ()=>{
-  const TestComponent = () => {
-    const c = useReactAuthKit();
-    return <div id="test"> {JSON.stringify(c.value)} </div>;
-  };
+// test('Testing Context Workflow', ()=>{
+//   const TestComponent = () => {
+//     const c = useReactAuthKit();
+//     return <div id="test"> {JSON.stringify(c.value)} </div>;
+//   };
 
+//   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+//       '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+//       '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
+//   Cookies.set('__', token);
+
+//   const tokenObject = new TokenObject<object>(
+//       '__',
+//       'cookie',
+//       null,
+//       false,
+//       window.location.hostname,
+//       window.location.protocol === 'https:',
+//   );
+
+//   render(
+//       // @ts-expect-error 
+//       <AuthKitContext.Provider value={{token: tokenObject, router: undefined, config: {fallbackPath: '/login'}}}>
+//         <TestComponent/>
+//       </AuthKitContext.Provider>,
+//   );
+
+//   const data = document.querySelector('#test');
+//   expect(data?.innerHTML).toEqual(` ${JSON.stringify(tokenObject.value)} `);
+// });
+
+// test('Testing Context config Workflow', ()=>{
+//   const TestComponent = () => {
+//     const c = useReactAuthKitConfig();
+//     return <div id="test"> {JSON.stringify(c)} </div>;
+//   };
+
+//   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+//       '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+//       '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
+//   Cookies.set('__', token);
+
+//   const tokenObject = new TokenObject<object>(
+//       '__',
+//       'cookie',
+//       null,
+//       false,
+//       window.location.hostname,
+//       window.location.protocol === 'https:',
+//   );
+
+//   render(
+//     // @ts-expect-error 
+//     <AuthKitContext.Provider value={{token: tokenObject, router: undefined, config: {fallbackPath: '/login'}}}>
+//       <TestComponent/>
+//     </AuthKitContext.Provider>,
+//   );
+
+//   const data = document.querySelector('#test');
+//   expect(data?.innerHTML).toEqual(` ${JSON.stringify({fallbackPath: '/login'})} `);
+// });
+
+describe('Context Workflow', ()=>{
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
-      '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
-      '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
-  Cookies.set('__', token);
+    '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+    '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
+  let tokenObject: TokenObject<object>;
+  let AuthProvider: React.FC<React.PropsWithChildren>;
+  
+  const navigate_fn = jest.fn();
 
-  const tokenObject = new TokenObject<object>(
+  beforeEach(()=>{
+    Cookies.set('__', token);
+    tokenObject = new TokenObject<object>(
       '__',
       'cookie',
       null,
       false,
       window.location.hostname,
       window.location.protocol === 'https:',
-  );
+    );
 
-  render(
-      <AuthKitContext.Provider value={tokenObject as any}>
+    const ReactRouterPlugin: Router = {
+      navigate: navigate_fn,
+      useNavigate: function(): ({to}: { to: string; }) => void {
+        return jest.fn();
+      },
+      usePath: function(): () => string {
+        return jest.fn()
+      },
+    };
+    
+    AuthProvider = ({children}) => (
+    // @ts-expect-error
+    <AuthKitContext.Provider value={{token: tokenObject, router: ReactRouterPlugin, config: {fallbackPath: '/login'}}}>
+      {children}
+    </AuthKitContext.Provider>)
+
+  });
+
+  it('useReactAuthKit', ()=>{
+    const TestComponent = () => {
+      const c = useReactAuthKit();
+      return <div id="test"> {JSON.stringify(c.value)} </div>;
+    };
+
+    render(
+      <AuthProvider>
         <TestComponent/>
-      </AuthKitContext.Provider>,
-  );
+      </AuthProvider>
+    );
 
-  const data = document.querySelector('#test');
-  expect(data?.innerHTML).toEqual(` ${JSON.stringify(tokenObject.value)} `);
+    const data = document.querySelector('#test');
+    expect(data?.innerHTML).toEqual(` ${JSON.stringify(tokenObject.value)} `);
+  });
+
+  it('useReactAuthKitConfig', ()=>{
+    const TestComponent = () => {
+      const c = useReactAuthKitConfig();
+      return <div id="test"> {JSON.stringify(c)} </div>;
+    };
+
+    render(
+      <AuthProvider>
+        <TestComponent/>
+      </AuthProvider>
+    );
+
+    const data = document.querySelector('#test');
+    expect(data?.innerHTML).toEqual(` ${JSON.stringify({fallbackPath: '/login'})} `);
+  });
+
+  it('useReactAuthKitConfig', ()=>{
+    const TestComponent = () => {
+      const c = useReactAuthKitRouter();
+      c?.navigate({to:'/hello'})
+      return <div id="test"> Hi </div>;
+    };
+
+    render(
+      <AuthProvider>
+        <TestComponent/>
+      </AuthProvider>
+    );
+    
+    expect(navigate_fn).toHaveBeenCalled()
+    expect(navigate_fn).toHaveBeenCalledWith({to:'/hello'})
+    
+  });
 });
 
-test('Throws error without AuthKitContext.Provider', ()=>{
-  const TestComponent = () => {
-    const c = useReactAuthKit();
-    return <div id="test"> {JSON.stringify(c.value)} </div>;
-  };
-  expect(() => render(
-      <TestComponent/>,
-  )).toThrow(AuthError);
+describe('Throws error without AuthKitContext.Provider', ()=>{
+  it('useReactAuthKit', ()=>{
+    const TestComponent = () => {
+      const c = useReactAuthKit();
+      return <div id="test"> {JSON.stringify(c.value)} </div>;
+    };
+    expect(() => render(
+        <TestComponent/>,
+    )).toThrow(AuthError);
+  });
+  
+  it('useReactAuthKitRouter', ()=>{
+    const TestComponent = () => {
+      useReactAuthKitRouter();
+      return <div id="test">Hi</div>;
+    };
+    expect(() => render(
+        <TestComponent/>,
+    )).toThrow(AuthError);
+  });
+  
+  it('useReactAuthKitConfig', ()=>{
+    const TestComponent = () => {
+      useReactAuthKitConfig();
+      return <div id="test">Hi</div>;
+    };
+    expect(() => render(
+        <TestComponent/>,
+    )).toThrow(AuthError);
+  });
 });
