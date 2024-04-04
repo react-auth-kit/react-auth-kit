6026880804c3069ace0bfffc190b0799b6a0712e
Arkadip Bhattacharya <hi@arkadip.dev>
2024-04-04 16:53:03 +0000

    test(hooks.signin): new test cases for navigations are done

--- packages/react-auth-kit/src/__tests__/hooks/useSignIn.test.tsx
+++ packages/react-auth-kit/src/__tests__/hooks/useSignIn.test.tsx
@@ -5,6 +5,7 @@ import AuthContext from '../../AuthContext';
 import TokenObject from '../../RxTokenObject';
 import * as reducers from '../../utils/reducers';
 import {AuthError} from '../../errors';
+import type Router from '../../route';
 
 const spy = jest.spyOn(reducers, 'doSignIn');
 
@@ -20,7 +21,7 @@ describe('useSignIn', () => {
           window.location.protocol === 'https:',
       );
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={tokenObject}>
+        <AuthContext.Provider value={{token: tokenObject, config:{}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -47,7 +48,7 @@ describe('useSignIn', () => {
           window.location.protocol === 'https:',
       );
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={tokenObject}>
+        <AuthContext.Provider value={{token: tokenObject, config:{}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -66,6 +67,36 @@ describe('useSignIn', () => {
       })).toThrow(AuthError);
       expect(spy).not.toHaveBeenCalled();
     });
+
+    it('navigateTo is specified, but Plugin is not integrated', ()=>{
+      const tokenObject = new TokenObject<unknown>(
+          '__',
+          'cookie',
+          '__refresh',
+          false,
+          window.location.hostname,
+          window.location.protocol === 'https:',
+      );
+      const wrapper = ({children}: {children: React.ReactNode}) => (
+        <AuthContext.Provider value={{token: tokenObject, config:{}}}>
+          {children}
+        </AuthContext.Provider>
+      );
+
+      const {result} = renderHook(() => useSignIn(), {wrapper});
+
+      expect(() => result.current({
+        auth: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+          '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+          '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        },
+        refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+        '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+        '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        navigateTo: '/secure'
+      })).toThrow(AuthError);
+    });
   });
 
   describe('Proper Sign In condition', ()=>{
@@ -79,7 +110,7 @@ describe('useSignIn', () => {
           window.location.protocol === 'https:',
       );
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={tokenObject}>
+        <AuthContext.Provider value={{token: tokenObject, config:{}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -115,7 +146,7 @@ describe('useSignIn', () => {
           window.location.protocol === 'https:',
       );
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={tokenObject}>
+        <AuthContext.Provider value={{token: tokenObject, config:{}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -146,5 +177,127 @@ describe('useSignIn', () => {
           },
       );
     });
+  });
+
+  describe('Proper Redirection is working without Refresh token', ()=>{
+    let navigate_fn: jest.Mock<any, any, any>;
+    const ReactRouterPlugin: Router = {
+      navigate: jest.fn(),
+      useNavigate: function(): ({to}: { to: string; }) => void {
+        return navigate_fn;
+      },
+      usePath: function(): () => string {
+        return jest.fn()
+      },
+    };
+
+    const tokenObject = new TokenObject<unknown>(
+      '__',
+      'cookie',
+      null,
+      false,
+      window.location.hostname,
+      window.location.protocol === 'https:',
+    );
+
+    const AuthProvider = ({children} : {children: React.ReactNode}) => (
+      <AuthContext.Provider value={{token: tokenObject, router: ReactRouterPlugin, config: {fallbackPath: '/login'}}}>
+        {children}
+      </AuthContext.Provider>)
+
+    beforeEach(()=>{
+      navigate_fn = jest.fn();
+    });
+
+    it('navigateTo param is specified', ()=>{
+      const {result} = renderHook(() => useSignIn(), {wrapper: AuthProvider});
+      expect(() => result.current({
+        auth: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+          '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+          '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        },
+        navigateTo: '/secure'
+      })).not.toThrow(AuthError);
+      expect(navigate_fn).toHaveBeenCalled();
+      expect(navigate_fn).toHaveBeenCalledWith({to: '/secure'})
+    });
+
+    it('navigateTo param is not specified', ()=>{
+      const {result} = renderHook(() => useSignIn(), {wrapper: AuthProvider});
+      expect(() => result.current({
+        auth: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+          '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+          '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        }
+      })).not.toThrow(AuthError);
+      expect(navigate_fn).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Proper Redirection is working With Refresh Token', ()=>{
+    let navigate_fn: jest.Mock<any, any, any>;
+    const ReactRouterPlugin: Router = {
+      navigate: jest.fn(),
+      useNavigate: function(): ({to}: { to: string; }) => void {
+        return navigate_fn;
+      },
+      usePath: function(): () => string {
+        return jest.fn()
+      },
+    };
+
+    const tokenObject = new TokenObject<unknown>(
+      '__',
+      'cookie',
+      '__refresh',
+      false,
+      window.location.hostname,
+      window.location.protocol === 'https:',
+    );
+
+    const AuthProvider = ({children} : {children: React.ReactNode}) => (
+      <AuthContext.Provider value={{token: tokenObject, router: ReactRouterPlugin, config: {fallbackPath: '/login'}}}>
+        {children}
+      </AuthContext.Provider>)
+      
+    beforeEach(()=>{
+      navigate_fn = jest.fn();
+    });
+
+    it('navigateTo param is specified', ()=>{
+      const {result} = renderHook(() => useSignIn(), {wrapper: AuthProvider});
+      expect(() => result.current({
+        auth: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+          '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+          '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        },
+        refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+        '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+        '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        navigateTo: '/secure'
+      })).not.toThrow(AuthError);
+      expect(navigate_fn).toHaveBeenCalled();
+      expect(navigate_fn).toHaveBeenCalledWith({to: '/secure'})
+    });
+
+    it('navigateTo param is not specified', ()=>{
+      const {result} = renderHook(() => useSignIn(), {wrapper: AuthProvider});
+      expect(() => result.current({
+        auth: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+          '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+          '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        },
+        refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+        '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+        '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8'
+      })).not.toThrow(AuthError);
+      expect(navigate_fn).not.toHaveBeenCalled();
+    });
+
+
   });
 });
