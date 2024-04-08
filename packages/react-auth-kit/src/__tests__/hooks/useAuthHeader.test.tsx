80140b3c3a54e7a1855d5ca4beb6ac2710971320
Arkadip Bhattacharya <hi@arkadip.dev>
2024-04-08 15:47:52 +0000

    test(hooks.authHeader): added new test cases

--- packages/react-auth-kit/src/__tests__/hooks/useAuthHeader.test.tsx
+++ packages/react-auth-kit/src/__tests__/hooks/useAuthHeader.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Cookies from 'js-cookie';
 import {renderHook} from '@testing-library/react';
-import useAuthHeader from '../../hooks/useAuthHeader';
+
+import * as AC from '../../AuthContext';
 import AuthContext from '../../AuthContext';
 import TokenObject from '../../RxTokenObject';
-import Cookies from 'js-cookie';
+import useAuthHeader from '../../hooks/useAuthHeader';
+
 
 describe('useAuthHeader', () => {
   it('should return an empty string if the user is not in the context', () => {
@@ -17,16 +20,16 @@ describe('useAuthHeader', () => {
     );
 
     const wrapper = ({children}: {children: React.ReactNode}) => (
-      <AuthContext.Provider value={tokenObject}>
+      <AuthContext.Provider value={{token: tokenObject, config: {}}}>
         {children}
       </AuthContext.Provider>
     );
 
     const {result} = renderHook(() => useAuthHeader(), {wrapper});
-    expect(result.current).toBeNull();
+    expect(result.current()).toBeNull();
   });
 
-  it('should return the auth header if it is available in the context', () => {
+  it('should return an empty string if the user is not in the context', () => {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
       '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
       '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
@@ -44,13 +47,56 @@ describe('useAuthHeader', () => {
     );
 
     const wrapper = ({children}: {children: React.ReactNode}) => (
-      <AuthContext.Provider value={tokenObject}>
+      <AuthContext.Provider value={{token: tokenObject, config: {}}}>
         {children}
       </AuthContext.Provider>
     );
 
     const {result} = renderHook(() => useAuthHeader(), {wrapper});
-    expect(result.current).toBe(`Bearer ${token}`);
+    expect(result.current()).toBe(`Bearer ${token}`);
+  });
+
+  it('the auth is there and expired', () => {
+    const spy = jest.fn()
+    const mockUseReactAuthKit = jest.spyOn(AC, 'useReactAuthKit')
+    // @ts-ignore
+    mockUseReactAuthKit.mockImplementation(()=>(
+      {
+        value: {
+          auth: {
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY'+
+            '3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjoxMDA4NjA1MTk1fQ.zII9AZg'+
+            'jgXMlfuNmN7dx-v-ROl3vl4eJdFc_3XfLWbs',
+            type: 'Bearer',
+            expiresAt: new Date(1008605195)
+          },
+          userState: {"name":"react"},
+          isSignIn: true,
+          isUsingRefreshToken: false,
+        }, 
+        set: spy
+      }
+    ))
+
+    const tokenObject = new TokenObject<unknown>(
+      '__',
+      'cookie',
+      null,
+      false,
+      window.location.hostname,
+      window.location.protocol === 'https:',
+    );
+    const wrapper = ({children}: {children: React.ReactNode}) => (
+      <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+        {children}
+      </AuthContext.Provider>
+    );
+
+    const {result} = renderHook(() => useAuthHeader(), {wrapper});
+    expect(result.current()).toBeNull();
+    expect(spy).toHaveBeenCalled();
+
+    mockUseReactAuthKit.mockRestore()
   });
 });
 
