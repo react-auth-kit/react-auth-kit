23fbf85ab4b6385c486b290e09b8037094fbfc87
Arkadip Bhattacharya <hi@arkadip.dev>
2024-05-03 13:19:50 +0000

    test(react-auth-kit): refresh boilerplate done

---
+++ packages/react-auth-kit/src/__tests__/refresh.test.tsx
@@ -0,0 +1,32 @@
+describe("Initial Refresh", ()=>{
+    it('If all tokens are present, do not refresh', ()=>{
+
+    });
+    it('If refresh condition is there and if the refresh component is there, render inital refresh component and api call and reducer call and state update', ()=>{
+
+    });
+    it('If refresh condition is there and if the refresh component is there, only api call and reducer call and state update',()=>{
+
+    });
+    it('If no refresh token is there, do not refresh, and call signOut', ()=>{
+
+    });
+    it('If the refresh component is called once, then it should not called multiple times', ()=>{
+
+    });
+});
+
+describe('Pariodic refresh', ()=>{
+    it('If signIn, the pariodic refresh is running, api is calling and reducer is called', ()=>{
+
+    });
+    it('If signout, nothing should happended', ()=>{
+
+    });
+});
+
+describe('Initial Refresh and Pariodic refresh',()=>{
+    it('Check the initial refresh in done and then pariodic refresh is working', ()=>{
+
+    });
+})
