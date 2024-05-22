c1a64448e3ea5f8a5ddc0530f24ebea84076d274
Arkadip Bhattacharya <hi@arkadip.dev>
2024-05-22 12:35:49 +0000

    test(react-auth-kit): refresh

--- packages/react-auth-kit/src/__tests__/refresh.test.tsx
+++ packages/react-auth-kit/src/__tests__/refresh.test.tsx
@@ -1,11 +1,13 @@
 // import { ReactNode } from "react";
 import Cookies from "js-cookie";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 
 import Refresh from "../Refresh";
-import createRefresh from "../createRefresh";
+import createRefresh, { RefreshTokenCallbackResponse } from "../createRefresh";
 import createStore from "../createStore";
 import * as reducers from '../utils/reducers';
+
+jest.useFakeTimers()
 
 const doSignOutSpy = jest.spyOn(reducers, 'doSignOut');
 const doRefreshSpy = jest.spyOn(reducers, 'doRefresh');
@@ -22,6 +24,8 @@ describe("Initial Refresh", () => {
 		Cookies.remove('___type');
 		Cookies.remove('___state');
 		Cookies.remove('___refresh');
+		doRefreshSpy.mockClear();
+		doSignOutSpy.mockClear();
 	});
 
 	it('If all tokens are present, do not refresh', () => {
@@ -68,59 +72,66 @@ describe("Initial Refresh", () => {
 		expect(doRefreshSpy).not.toHaveBeenCalled();
 	});
 	
-	// describe('If refresh condition is there and if the refresh component is there, render inital refresh component and api call and reducer call and state update', () => {
-	// 	it('API call is successfull', () => {
-	// 		const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
-	// 		'0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw603AjpA'+
-	// 		'qNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
-	// 		Cookies.set('___refresh', refreshToken);
+	describe('If refresh condition is there and if the refresh component is there, render inital refresh component and api call and reducer call and state update', () => {
+		it('API call is successfull', async () => {
+			const refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+			'0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw603AjpA'+
+			'qNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
+			Cookies.set('___refresh', refreshToken);
 	
 	
-	// 		const api = jest.fn(
-	// 			() => new Promise<RefreshTokenCallbackResponse<unknown>>((resolve) => {
-	// 				resolve({
-	// 					newAuthToken: 'asdf',
-	// 					isSuccess: true
-	// 				})
-	// 			})
-	// 		);
-	// 		const createRefreshData = createRefresh({
-	// 			interval: 10,
-	// 			refreshApiCallback: api,
-	// 			initalRefreshComponent: <InitialRefreshData/>
-	// 		});
+			const api = jest.fn(
+				() => new Promise<RefreshTokenCallbackResponse<unknown>>((resolve) => {
+					setTimeout(resolve, 50, {
+						newAuthToken: 'asdf',
+						isSuccess: true
+					})
+				})
+			);
+
+			const createRefreshData = createRefresh({
+				interval: 10,
+				refreshApiCallback: api,
+				initalRefreshComponent: <InitialRefreshData/>
+			});
 	
-	// 		const store = createStore({
-	// 			authName: '__',
-	// 			authType: 'cookie',
-	// 			cookieDomain: window.location.hostname,
-	// 			cookieSecure: window.location.protocol === 'https:',
-	// 			refresh: createRefreshData,
-	// 			debug: true
-	// 		});
+			const store = createStore({
+				authName: '__',
+				authType: 'cookie',
+				cookieDomain: window.location.hostname,
+				cookieSecure: window.location.protocol === 'https:',
+				refresh: createRefreshData,
+				debug: true
+			});
 	
-	// 		const {container} = render(
-	// 			// @ts-expect-error refresh type
-	// 			<Refresh refresh={store.refresh} store={store.tokenObject}>
-	// 				<div>
-	// 					Container
-	// 				</div>
-	// 			</Refresh>
-	// 		);
+			const {container} = render(
+				// @ts-expect-error refresh type
+				<Refresh refresh={store.refresh} store={store.tokenObject}>
+					<div>
+						Container
+					</div>
+				</Refresh>
+			);
 	
-	// 		expect(container).toMatchSnapshot();
-	// 		expect(api).toHaveBeenCalled();
-	// 		expect(doSignOutSpy).not.toHaveBeenCalled();
-	// 		expect(doRefreshSpy).toHaveBeenCalled();
-	// 	});
-	// 	it('API call is not successfull', () => {
-	// 		// const api = jest.fn(
-	// 		// 	() => new Promise((resolve, reject) => {
-	// 		// 		reject('error');
-	// 		// 	})
-	// 		// );
-	// 	});
-	// });
+			expect(container).toMatchSnapshot();
+
+			jest.advanceTimersByTime(55);
+			
+			await waitFor(() => expect(api).toHaveBeenCalledTimes(1))
+
+			expect(container).toMatchSnapshot();
+			expect(doSignOutSpy).not.toHaveBeenCalled();
+			expect(doRefreshSpy).toHaveBeenCalled();
+
+		});
+		it('API call is not successfull', () => {
+			// const api = jest.fn(
+			// 	() => new Promise((resolve, reject) => {
+			// 		reject('error');
+			// 	})
+			// );
+		});
+	});
 	
 	// describe('If refresh condition is there and if the refresh component is there, only api call and reducer call and state update', () => {
 	// 	it('API call is successfull', () => {
