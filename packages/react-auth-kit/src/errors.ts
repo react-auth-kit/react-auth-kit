82bedefc07de830b5a7646e8e9a05fde3a55bd38
Arkadip <in2arkadipb13@gmail.com>
2025-07-11 10:45:21 +0530

    refactor(errors): remove unnecessary constructor from AuthError class

--- packages/react-auth-kit/src/errors.ts
+++ packages/react-auth-kit/src/errors.ts
@@ -2,10 +2,4 @@
  * Base Error Class for all the auth kit related errors.
  */
 export class AuthError extends Error {
-  /**
-   * @param message - Error Message to be shown to the developer.
-   */
-  constructor(message: string) {
-    super(message);
-  }
 }
