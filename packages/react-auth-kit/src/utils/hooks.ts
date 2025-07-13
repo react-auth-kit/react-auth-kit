dc694eec08570cec6c6433ec9e2a7cc7b773baec
Arkadip <in2arkadipb13@gmail.com>
2025-07-13 21:15:18 +0530

    refactor(utils): add useNavigateTo and useTryNavigateTo hooks, update useInterval typing and imports

--- packages/react-auth-kit/src/utils/hooks.ts
+++ packages/react-auth-kit/src/utils/hooks.ts
@@ -1,7 +1,7 @@
 'use client';
 
-import {useRef, useEffect} from 'react';
-import type {MutableRefObject} from 'react';
+import {useRef, useEffect, RefObject} from 'react';
+import {useReactAuthKitRouter} from "../AuthContext";
 
 
 /**
@@ -14,7 +14,7 @@ import type {MutableRefObject} from 'react';
  * @returns - The Reference of the `setInterval` function
  */
 function useInterval(callback: ()=>void, delay:number|null)
-  : MutableRefObject<number | null> {
+  : RefObject<number | null> {
   const savedCallback = useRef(callback);
   const intervalRef = useRef<number | null>(null);
 
@@ -41,4 +41,51 @@ function useInterval(callback: ()=>void, delay:number|null)
   return intervalRef;
 }
 
-export {useInterval};
+/**
+ * @internal
+ *
+ * React Hook to navigate to a specific path using the router plugin
+ *
+ * @returns - A function that navigates to the specified path
+ *
+ * @throws Error if the router plugin is not implemented in the AuthProvider
+ */
+function useNavigateTo(): (to: string) => void {
+  const router = useReactAuthKitRouter();
+  const navigate = router ? router.useNavigate() : null;
+
+  /**
+   * Navigate to a specific path
+   *
+   * @param to - The path to navigate to
+   * @throws Error if the router plugin is not implemented in the AuthProvider
+   */
+  return (to: string) => {
+    if (router && navigate) {
+      navigate({to});
+    } else {
+      throw new Error(
+        'Router Plugin is not implemented in the AuthProvider. Please use the router prop of AuthProvider and Router plugin to use this feature',
+      );
+    }
+  };
+}
+
+/**
+ * @internal
+ *
+ * React Hook to navigate to a specific path if the path is defined
+ *
+ * @returns - A function that navigates to the specified path if it is defined
+ */
+function useTryNavigateTo() {
+  const navigateTo = useNavigateTo();
+
+  return (to: string | undefined | null): void => {
+    if (to) {
+      navigateTo(to);
+    }
+  };
+}
+
+export {useInterval, useNavigateTo, useTryNavigateTo};
