c4c004d732fdb14e6c5f2d29c7bc4dd3dcd31e2e
Arkadip <in2arkadipb13@gmail.com>
2025-07-13 21:02:04 +0530

    refactor(auth): update useSignOut hook to use useReactAuthKitStore and enhance documentation

--- packages/react-auth-kit/src/hooks/useSignOut.ts
+++ packages/react-auth-kit/src/hooks/useSignOut.ts
@@ -1,8 +1,8 @@
 'use client';
 
-import {doSignOut} from '../utils/reducers';
-import {useReactAuthKit, useReactAuthKitRouter} from '../AuthContext';
-import {BaseAuthKitError} from "../error/BaseAuthKitError";
+import {useReactAuthKitStore, useReactAuthKitRouter} from '../AuthContext';
+import {AuthKitError} from "../error";
+import Action from "../utils/action";
 
 /**
  * Sign Out React Hook
@@ -36,28 +36,71 @@ import {BaseAuthKitError} from "../error/BaseAuthKitError";
  * navigate("/login")
  * ```
  *
- * @throws BaseAuthKitError
+ * @throws AuthKitError
  * Thrown if the Hook is used outside the Provider Scope
  *
  */
-function useSignOut(): (navigateTo?: string) => void {
-  const context = useReactAuthKit();
 
+/**
+ * useSignOut React Hook
+ *
+ * This hook provides a function to sign out the user and optionally navigate to a specified path.
+ * It removes the authentication state from the store and, if a router is configured,
+ * navigates to the specified path.
+ *
+ * @param navigateTo - An optional string representing the path to navigate to after signing out.
+ *
+ * @returns A function that, when called, signs out the user and navigates to the specified path if provided.
+ *
+ * @example
+ * Simple example:
+ * ```js
+ * import useSignOut from 'react-auth-kit/hooks/useSignOut'
+ *
+ * const SecureComponent = () => {
+ *  const signOut = useSignOut();
+ *  return (
+ *    <button onClick={() => signOut()}>Sign Out!</button>
+ *  )
+ * }
+ * ```
+ *
+ * @example
+ * Example with navigation:
+ * ```js
+ * import useSignOut from 'react-auth-kit/hooks/useSignOut'
+ *
+ * const SecureComponent = () => {
+ *  const signOut = useSignOut("/login"); // <-- Specify the path to navigate after signing out
+ *  return (
+ *    <button onClick={() => signOut()}>Sign Out and Redirect to Log in</button>
+ *  )
+ * };
+ * ```
+ */
+function useSignOut(navigateTo?: string): (navigateTo?: string) => void {
+  const store = useReactAuthKitStore();
   const router = useReactAuthKitRouter();
+
   const navigate = router ? router.useNavigate() : null;
 
-
-  return (navigateTo?: string) => {
-    context.set(doSignOut());
+  /**
+   * Sign out function
+   * This function signs out the user by removing the authentication state
+   * from the store and optionally navigates to a specified path.
+   *
+   */
+  return () :void => {
+    Action.doSignOut(store)
     if (navigateTo) {
       if (router && navigate) {
         navigate({to: navigateTo});
       } else {
         throw new
-        BaseAuthKitError(
+        AuthKitError(
             'Router Plugin is not implemented in the AuthProvider. Please'+
               ' use the router prop of AuthProvider and Router plugin to'+
-              ' use this feture',
+              ' use this feature',
         );
       }
     }
