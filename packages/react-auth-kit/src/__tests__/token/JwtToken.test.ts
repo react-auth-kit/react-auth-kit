f2dc37a940e0c2f7443a3d0596bdd4d2bebe19e8
Arkadip <in2arkadipb13@gmail.com>
2025-07-15 11:10:30 +0530

    test(JwtToken): add unit test for JWT expiration parsing
    - Added test to verify `getExpiresAt` method parses JWT and returns the correct expiration date.

---
+++ packages/react-auth-kit/src/__tests__/token/JwtToken.test.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025 Arkadip Bhattacharya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import JwtToken from "../../token/JwtToken";
+import {IToken} from "../../token";
+
+describe('JWT Token', () => {
+  const token: IToken = new JwtToken();
+
+  it('should parse JWT and return expiration date', () => {
+    const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
+      '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
+      '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
+    expect(token.getExpiresAt(jwt)).toStrictEqual(new Date(8008605195000));
+  });
+});
