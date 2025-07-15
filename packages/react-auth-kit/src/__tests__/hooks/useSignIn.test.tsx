7dc842d158b7110457c36a3829b7df154f37dd15
Arkadip <in2arkadipb13@gmail.com>
2025-07-15 10:29:20 +0530

    test(useSignIn): refactor tests to use createCookieTokenStore
    - Replaced `TokenObject` with `createCookieTokenStore` for consistency with updated token store implementation.
    - Adjusted mocks, context providers, and error handling to align with the new structure.
    - Updated references to replace `BaseAuthKitError` with `AuthKitError`.

--- packages/react-auth-kit/src/__tests__/hooks/useSignIn.test.tsx
+++ packages/react-auth-kit/src/__tests__/hooks/useSignIn.test.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import {renderHook} from '@testing-library/react';
 
-import type Router from '../../route';
-
 import AuthContext from '../../AuthContext';
-import TokenObject from '../../RxTokenObject';
 import useSignIn from '../../hooks/useSignIn';
-import * as reducers from '../../utils/reducers';
-import {BaseAuthKitError} from "../../error/BaseAuthKitError";
+import Action from '../../utils/action';
+import {AuthKitError} from "../../error";
+import {IRouter} from "../../route";
+import {createCookieTokenStore} from "../helpers/storeCreation";
 
-const spy = jest.spyOn(reducers, 'doSignIn');
+const signInActionSpy = jest.spyOn(Action, "doSignIn");
 
 describe('useSignIn', () => {
   describe('Check error conditions', ()=>{
     it('Is refresh is on, but no refresh is specified', ()=>{
-      const tokenObject = new TokenObject<unknown>(
-          '__',
-          'cookie',
-          '___refresh',
-          false,
-          window.location.hostname,
-          window.location.protocol === 'https:',
-      );
+      const tokenObject = createCookieTokenStore("__", true);
+
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+        <AuthContext.Provider value={{store: tokenObject, config: {}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -36,21 +29,14 @@ describe('useSignIn', () => {
           '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
           '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
         },
-      })).toThrow(BaseAuthKitError);
-      expect(spy).not.toHaveBeenCalled();
+      })).toThrow(AuthKitError);
+      expect(signInActionSpy).not.toHaveBeenCalled();
     });
 
     it('Is refresh is off, but refresh is specified', ()=>{
-      const tokenObject = new TokenObject<unknown>(
-          '__',
-          'cookie',
-          null,
-          false,
-          window.location.hostname,
-          window.location.protocol === 'https:',
-      );
+      const tokenObject = createCookieTokenStore("__", false);
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+        <AuthContext.Provider value={{store: tokenObject, config: {}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -66,21 +52,14 @@ describe('useSignIn', () => {
         refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
         '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
         '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
-      })).toThrow(BaseAuthKitError);
-      expect(spy).not.toHaveBeenCalled();
+      })).toThrow(AuthKitError);
+      expect(signInActionSpy).not.toHaveBeenCalled();
     });
 
     it('navigateTo is specified, but Plugin is not integrated', ()=>{
-      const tokenObject = new TokenObject<unknown>(
-          '__',
-          'cookie',
-          '__refresh',
-          false,
-          window.location.hostname,
-          window.location.protocol === 'https:',
-      );
+      const tokenObject = createCookieTokenStore("__", false);
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+        <AuthContext.Provider value={{store: tokenObject, config: {}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -97,22 +76,15 @@ describe('useSignIn', () => {
         '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
         '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
         navigateTo: '/secure',
-      })).toThrow(BaseAuthKitError);
+      })).toThrow(AuthKitError);
     });
   });
 
   describe('Proper Sign In condition', ()=>{
     it('Without refresh token', ()=> {
-      const tokenObject = new TokenObject<unknown>(
-          '__',
-          'cookie',
-          null,
-          false,
-          window.location.hostname,
-          window.location.protocol === 'https:',
-      );
+      const tokenObject = createCookieTokenStore("__", false);
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+        <AuthContext.Provider value={{store: tokenObject, config: {}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -125,30 +97,24 @@ describe('useSignIn', () => {
           '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
           '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
         },
-      })).not.toThrow(BaseAuthKitError);
-      expect(spy).toHaveBeenCalled();
-      expect(spy).toHaveBeenCalledWith(
-          {
-            auth: {
-              token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMj'+
-            'M0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ'+
-            '.ijw603AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
-            },
+      })).not.toThrow(AuthKitError);
+      expect(signInActionSpy).toHaveBeenCalled();
+      expect(signInActionSpy).toHaveBeenCalledWith(
+        {
+          auth: {
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMj'+
+          'M0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ'+
+          '.ijw603AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
           },
+        },
+        tokenObject,
       );
     });
 
     it('With refresh token', ()=> {
-      const tokenObject = new TokenObject<unknown>(
-          '__',
-          'cookie',
-          '___refresh',
-          false,
-          window.location.hostname,
-          window.location.protocol === 'https:',
-      );
+      const tokenObject = createCookieTokenStore("__", true);
       const wrapper = ({children}: {children: React.ReactNode}) => (
-        <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+        <AuthContext.Provider value={{store: tokenObject, config: {}}}>
           {children}
         </AuthContext.Provider>
       );
@@ -164,26 +130,27 @@ describe('useSignIn', () => {
         refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
         '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
         '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
-      })).not.toThrow(BaseAuthKitError);
-      expect(spy).toHaveBeenCalled();
-      expect(spy).toHaveBeenCalledWith(
-          {
-            auth: {
-              token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMj'+
-            'M0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ'+
-            '.ijw603AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
-            },
-            refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMj'+
+      })).not.toThrow(AuthKitError);
+      expect(signInActionSpy).toHaveBeenCalled();
+      expect(signInActionSpy).toHaveBeenCalledWith(
+        {
+          auth: {
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMj'+
           'M0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ'+
           '.ijw603AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
           },
+          refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMj'+
+        'M0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ'+
+        '.ijw603AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
+        },
+        tokenObject
       );
     });
   });
 
   describe('Proper Redirection is working without Refresh token', ()=>{
     let navigateFn: jest.Mock<any, any, any>;
-    const ReactRouterPlugin: Router = {
+    const ReactRouterPlugin: IRouter = {
       navigate: jest.fn(),
       useNavigate: function(): ({to}: { to: string; }) => void {
         return navigateFn;
@@ -193,19 +160,12 @@ describe('useSignIn', () => {
       },
     };
 
-    const tokenObject = new TokenObject<unknown>(
-        '__',
-        'cookie',
-        null,
-        false,
-        window.location.hostname,
-        window.location.protocol === 'https:',
-    );
+    const tokenObject = createCookieTokenStore("__", false);
 
     const AuthProvider = ({children} : {children: React.ReactNode}) => (
       <AuthContext.Provider value={
         {
-          token: tokenObject,
+          store: tokenObject,
           router: ReactRouterPlugin,
           config: {fallbackPath: '/login'},
         }
@@ -226,7 +186,7 @@ describe('useSignIn', () => {
           '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
         },
         navigateTo: '/secure',
-      })).not.toThrow(BaseAuthKitError);
+      })).not.toThrow(AuthKitError);
       expect(navigateFn).toHaveBeenCalled();
       expect(navigateFn).toHaveBeenCalledWith({to: '/secure'});
     });
@@ -239,14 +199,14 @@ describe('useSignIn', () => {
           '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
           '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
         },
-      })).not.toThrow(BaseAuthKitError);
+      })).not.toThrow(AuthKitError);
       expect(navigateFn).not.toHaveBeenCalled();
     });
   });
 
   describe('Proper Redirection is working With Refresh Token', ()=>{
     let navigateFn: jest.Mock<any, any, any>;
-    const ReactRouterPlugin: Router = {
+    const ReactRouterPlugin: IRouter = {
       navigate: jest.fn(),
       useNavigate: function(): ({to}: { to: string; }) => void {
         return navigateFn;
@@ -256,19 +216,12 @@ describe('useSignIn', () => {
       },
     };
 
-    const tokenObject = new TokenObject<unknown>(
-        '__',
-        'cookie',
-        '__refresh',
-        false,
-        window.location.hostname,
-        window.location.protocol === 'https:',
-    );
+    const tokenObject = createCookieTokenStore("__", true);
 
     const AuthProvider = ({children} : {children: React.ReactNode}) => (
       <AuthContext.Provider value={
         {
-          token: tokenObject,
+          store: tokenObject,
           router: ReactRouterPlugin,
           config: {fallbackPath: '/login'},
         }
@@ -292,7 +245,7 @@ describe('useSignIn', () => {
         '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
         '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
         navigateTo: '/secure',
-      })).not.toThrow(BaseAuthKitError);
+      })).not.toThrow(AuthKitError);
       expect(navigateFn).toHaveBeenCalled();
       expect(navigateFn).toHaveBeenCalledWith({to: '/secure'});
     });
@@ -308,7 +261,7 @@ describe('useSignIn', () => {
         refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
         '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
         '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8',
-      })).not.toThrow(BaseAuthKitError);
+      })).not.toThrow(AuthKitError);
       expect(navigateFn).not.toHaveBeenCalled();
     });
   });
