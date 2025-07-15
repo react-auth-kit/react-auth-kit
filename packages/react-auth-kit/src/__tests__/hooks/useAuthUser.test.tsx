b18df181223c176ceac2d5b5f0d1659efa358318
Arkadip <in2arkadipb13@gmail.com>
2025-07-15 10:19:19 +0530

    test(useAuthUser): refactor tests to use createCookieTokenStore
    - Replaced `TokenObject` with `createCookieTokenStore` for consistency with updated token management.
    - Adjusted mocks, context providers, and cookie keys to reflect the new implementation.
    - Updated references to `useReactAuthKit` to align with `useReactAuthKitStore`.
    - Simplified test setup and enhanced clarity in test cases.

--- packages/react-auth-kit/src/__tests__/hooks/useAuthUser.test.tsx
+++ packages/react-auth-kit/src/__tests__/hooks/useAuthUser.test.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
 import Cookies from 'js-cookie';
 import {renderHook} from '@testing-library/react';
 
 import * as AC from '../../AuthContext';
 import AuthContext from '../../AuthContext';
-import TokenObject from '../../RxTokenObject';
 import useAuthUser from '../../hooks/useAuthUser';
+import {createCookieTokenStore} from "../helpers/storeCreation";
 
 describe('useAuthUser', () => {
   it('should return null if the user is not in authenticated', () => {
-    const tokenObject = new TokenObject<unknown>(
-        '__',
-        'cookie',
-        null,
-        false,
-        window.location.hostname,
-        window.location.protocol === 'https:',
-    );
+    const tokenObject = createCookieTokenStore("__", false);
 
     const wrapper = ({children}: {children: React.ReactNode}) => (
-      <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+      <AuthContext.Provider value={{store: tokenObject, config: {}}}>
         {children}
       </AuthContext.Provider>
     );
@@ -32,20 +24,13 @@ describe('useAuthUser', () => {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM'+
       '0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw60'+
       '3AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8';
-    Cookies.set('__', token);
-    Cookies.set('___type', 'Bearer');
+    Cookies.set('___auth', token);
+    Cookies.set('___auth_type', 'Bearer');
     Cookies.set('___state', '{"name":"react"}');
 
-    const tokenObject = new TokenObject<unknown>(
-        '__',
-        'cookie',
-        null,
-        false,
-        window.location.hostname,
-        window.location.protocol === 'https:',
-    );
+    const tokenObject = createCookieTokenStore("__", false);
     const wrapper = ({children}: {children: React.ReactNode}) => (
-      <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+      <AuthContext.Provider value={{store: tokenObject, config: {}}}>
         {children}
       </AuthContext.Provider>
     );
@@ -56,7 +41,7 @@ describe('useAuthUser', () => {
 
   it('the auth is there and expired', () => {
     const spy = jest.fn();
-    const mockUseReactAuthKit = jest.spyOn(AC, 'useReactAuthKit');
+    const mockUseReactAuthKit = jest.spyOn(AC, 'useReactAuthKitStore');
     // @ts-expect-error response type is missing
     mockUseReactAuthKit.mockImplementation(()=>(
       {
@@ -76,16 +61,9 @@ describe('useAuthUser', () => {
       }
     ));
 
-    const tokenObject = new TokenObject<unknown>(
-        '__',
-        'cookie',
-        null,
-        false,
-        window.location.hostname,
-        window.location.protocol === 'https:',
-    );
+    const tokenObject = createCookieTokenStore("__", false);
     const wrapper = ({children}: {children: React.ReactNode}) => (
-      <AuthContext.Provider value={{token: tokenObject, config: {}}}>
+      <AuthContext.Provider value={{store: tokenObject, config: {}}}>
         {children}
       </AuthContext.Provider>
     );
