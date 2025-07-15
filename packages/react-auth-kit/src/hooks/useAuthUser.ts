4fb3d355c3c9bc442ee14cd74e3db335c96c5ff5
Arkadip <in2arkadipb13@gmail.com>
2025-07-15 10:17:00 +0530

    fix(useAuthUser): update return type to handle unauthenticated state
    - Modified the `useAuthUser` hook to return `null` when the user is unauthenticated.
    - Adjusted type definition to reflect the possibility of a nullable return value.
    - Simplified examples for improved clarity.

--- packages/react-auth-kit/src/hooks/useAuthUser.ts
+++ packages/react-auth-kit/src/hooks/useAuthUser.ts
@@ -3,52 +3,6 @@
 import {useReactAuthKitStore} from '../AuthContext';
 import useIsAuthenticated from './useIsAuthenticated';
 
-/**
- * Auth User Data React Hook
- *
- * Call the hook
- * to get the authenticated user data into your React Component
- *
- * This uses the context data to determine the user data
- *
- * @typeParam T - Type of User State Object
- *
- * @returns React Hook with user state functionality.
- * If the user is authenticated, then user data is returned.
- * If the user is not authenticated, then `null` is returned.
- *
- * @throws AuthError
- * Thrown if the Hook is used outside the Provider Scope.
- *
- * @example
- * Here is the example of JavaScript
- * ```js
- * import useAuthUser from 'react-auth-kit/hooks/useAuthUser'
- *
- * const Component = () => {
- *  const authUser = useAuthUser()
- *  const name = authUser.name;
- *  const uuid = authUser.uuid;
- *  ...
- * }
- * ```
- * Here is the example of TypeScript
- * ```tsx
- * import useAuthUser from 'react-auth-kit/hooks/useAuthUser'
- *
- * interface IUserData {
- *  name: string;
- *  uuid: string;
- * };
- *
- * const Component = () => {
- *  const authUser = useAuthUser<IUserData>()
- *  const name = authUser.name;
- *  const uuid = authUser.uuid;
- *  ...
- * }
- * ```
- */
 
 /**
  * useAuthUser React Hook
@@ -65,6 +19,7 @@ import useIsAuthenticated from './useIsAuthenticated';
  * JavaScript Example:
  * ```js
  * import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
+ *
  * const Component = () => {
  *  const authUser = useAuthUser();
  *  const data = authUser();
@@ -86,18 +41,20 @@ import useIsAuthenticated from './useIsAuthenticated';
  * const Component = () => {
  *  const authUser = useAuthUser<IUserData>();
  *  const data = authUser();
+ *
  *  console.log(data.name); // Access username
  *  console.log(data.uuid); // Access user UUID
  * }
  * ```
  */
-function useAuthUser<T>(): () => T {
+function useAuthUser<T>(): () => T | null {
   const {value} = useReactAuthKitStore();
   const isAuthenticated = useIsAuthenticated();
 
   return () => {
-    isAuthenticated()
-    return value.userState as T;
+    return isAuthenticated() ?
+      value.userState as T :
+      null;
   };
 }
 
