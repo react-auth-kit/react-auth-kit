d8d9d81e46a3af44bb16bcd77190da76f4dbcea3
Arkadip <in2arkadipb13@gmail.com>
2025-07-24 23:07:07 +0530

    chore(ci): remove Codecov coverage upload step from the workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 const {
   defineConfig,
+  globalIgnores,
 } = require("eslint/config");
 
 const globals = require("globals");
@@ -8,48 +9,63 @@ const react = require("eslint-plugin-react");
 
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const tsdoc = require("eslint-plugin-tsdoc");
+const tsParser = require("@typescript-eslint/parser");
 
 
-
-module.exports = defineConfig([{
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.jest,
-      ...globals.node,
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      parser: tsParser,
+      "ecmaVersion": 2021,
+      "sourceType": "module",
     },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+
+    extends: [
+      "js/recommended",
+    ],
+
+    plugins: {
+      js,
+      react,
+      "@typescript-eslint": typescriptEslint,
+      tsdoc,
+    },
+
+    "settings": {
+      "react": {
+        "version": "detect",
       },
     },
-  },
 
-  extends: [
-    "js/recommended",
-  ],
-
-  plugins: {
-    js,
-    react,
-    "@typescript-eslint": typescriptEslint,
-    tsdoc,
-  },
-
-  "settings": {
-    "react": {
-      "version": "detect",
+    "rules": {
+      "tsdoc/syntax": "warn",
+      "react/prop-types": 0,
+      "react/display-name": 0,
+      "react/react-in-jsx-scope": "off",
+      "valid-jsdoc": 0,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
-
-  "rules": {
-    "tsdoc/syntax": "warn",
-    "react/prop-types": 0,
-    "react/display-name": 0,
-    "react/react-in-jsx-scope": "off",
-    "valid-jsdoc": 0,
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "@typescript-eslint/no-explicit-any": "off",
-  },
-}]);
+  globalIgnores([
+    "**/types",
+    "**/dist",
+    "**/node_modules",
+    "**/.idea",
+    "**/env",
+    "**/docs",
+    "**/docs_overrides",
+  ])
+]);
