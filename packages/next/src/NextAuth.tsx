dadb672076f5f1a855f52e746981d6f2668d5fcf
Arkadip <in2arkadipb13@gmail.com>
2025-07-26 21:36:53 +0530

    chore(next): fix typo in comment within `NextAuth.tsx`

--- packages/next/src/NextAuth.tsx
+++ packages/next/src/NextAuth.tsx
@@ -73,7 +73,7 @@ export default function NextAuth({fallbackPath, children}: NextAuthProps) {
 
   useEffect(() => {
     if (!isAuthenticated(store.value)) {
-      // Redirect them to the /login page, but save the current location they
+      // Redirect them to the /login page but save the current location they
       // were trying to go to when they were redirected. This allows us to
       // send them along to that page after they log in, which is a nicer
       // user experience than dropping them off on the home page.
