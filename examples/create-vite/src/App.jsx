4ec90c3e8d830e86b2b9cac14396212abdecb81f
Arkadip <in2arkadipb13@gmail.com>
2025-07-28 11:45:20 +0530

    chore(examples): add `create-vite` React + Vite example with authentication setup

--- examples/create-vite/src/App.jsx
+++ examples/create-vite/src/App.jsx
@@ -1,30 +1,28 @@
-import './App.css'
-import RoutesPage from './RoutesPage'
+import RouteComponent from './RouteComponent'
 import AuthProvider from 'react-auth-kit'
-import createStore from 'react-auth-kit/createStore';
+import createAuthStore from 'react-auth-kit/store/createAuthStore';
 import ReactRouterPlugin from '@auth-kit/react-router/route'
 import { BrowserRouter } from 'react-router-dom';
-import refreshApi from './refresh'
+import refreshApi from './refresh/refresh'
 
-const store = createStore({
+const store = createAuthStore("cookie", {
   authName:'_auth',
-  authType:'cookie',
   cookieDomain: window.location.hostname,
   cookieSecure: window.location.protocol === 'https:',
+  refresh: refreshApi,
   debug: true,
-  refresh: refreshApi
-})
+});
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <AuthProvider store={store} router={ReactRouterPlugin} fallbackPath='/login'>
-        <RoutesPage/>
-      </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider store={store} router={ReactRouterPlugin} fallbackPath='/login'>
+          <RouteComponent/>
+        </AuthProvider>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ examples/create-vite/src/refresh/refresh.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 Arkadip Bhattacharya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import createRefresh from 'react-auth-kit/refresh/createRefresh'
+import InitialRefresh from './InitialRefresh.js';
+
+const mock = new MockAdapter(axios, {delayResponse: 2000});
+
+mock.onPost("/refresh").reply(200, {
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo4MDA4NjA1MTk1fQ.ijw603AjpAqNwnUXmv6YB5L6m5aL-llIgBsTJo-k2r8"
+});
+
+const refreshApi = createRefresh({
+  interval: 10,
+  initialRefreshComponent: <InitialRefresh/>,
+  refreshApiCallback: async (param) => {
+    console.log(param)
+    try {
+      const response = await axios.post("/refresh", param, {
+        headers: {'Authorization': `Bearer ${param.authToken}`}}
+      )
+      return {
+        isSuccess: true,
+        newAuthToken: response.data.token,
+        newAuthTokenExpireIn: 10,
+        newRefreshTokenExpiresIn: 60
+
+      }
+    }
+    catch(error){
+      console.error(error)
+      return {
+        isSuccess: false
+      }
+    }
+  },
+})
+
+export default refreshApi
