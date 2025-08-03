1a6c9facbc021b21329b42230189d8d2d95e7c56
Arkadip <in2arkadipb13@gmail.com>
2025-08-03 22:56:07 +0530

    chore(react-router): remove the deprecated ` fallbackPath ` prop and simplify route components - `RequireAuth` - `AuthOutlet`

--- packages/react-router/src/AuthOutlet.tsx
+++ packages/react-router/src/AuthOutlet.tsx
@@ -1,27 +1,10 @@
 // noinspection JSDeprecatedSymbols
 
-import * as React from 'react';
-import {Navigate, Outlet} from 'react-router';
+import type {ReactNode} from "react";
+import {Outlet} from 'react-router';
 
-import {AuthKitConfigError} from 'react-auth-kit/error/AuthKitConfigError';
-import {useReactAuthKitConfig} from 'react-auth-kit/AuthContext';
 import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated';
 
-
-/**
- * Component Props for Auth Outlet
- */
-interface AuthOutletProps {
-  /**
-   * Path to redirect if the user is not authenticated
-   *
-   * @deprecated Use AuthProvider fallbackPath prop instead.
-   * Will be removed in the upcoming version
-   * @example
-   * `/login`
-   */
-  fallbackPath?: string
-}
 
 /**
  * AuthOutlet provides an easy solution to implement private
@@ -45,32 +28,8 @@ interface AuthOutletProps {
  * }
  * ```
  */
-function AuthOutlet({fallbackPath}: AuthOutletProps): React.ReactNode {
-  const isAuthenticated = useIsAuthenticated();
-  const config = useReactAuthKitConfig();
-
-  let fp;
-  if (fallbackPath !== undefined) {
-    fp = fallbackPath;
-  } else if (
-    config.fallbackPath !== undefined
-  ) {
-    fp = config.fallbackPath;
-  } else {
-    throw new AuthKitConfigError(
-      'fallbackPath prop must be present'+
-      ' in AuthProvider or AuthOutlet component',
-    );
-  }
-
-  if (!isAuthenticated()) {
-    // Redirect them to the /login page but save the current location they
-    // were trying to go to when they were redirected. This allows us to
-    // send them along to that page after they log in, which is a nicer
-    // user experience than dropping them off on the home page.
-    return <Navigate to={fp} replace />;
-  }
-
+function AuthOutlet(): ReactNode {
+  useIsAuthenticated();
   return <Outlet />;
 }
 
--- packages/react-router/src/RequireAuth.tsx
+++ packages/react-router/src/RequireAuth.tsx
@@ -1,10 +1,6 @@
 // noinspection JSDeprecatedSymbols
 
 import * as React from 'react';
-import {Navigate} from 'react-router';
-
-import {AuthKitConfigError} from 'react-auth-kit/error/AuthKitConfigError';
-import {useReactAuthKitConfig} from 'react-auth-kit/AuthContext';
 import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated';
 import {type ReactNode} from "react";
 
@@ -16,15 +12,6 @@ interface RequireAuthProps {
    * Children component which will require auth to access
    */
   children: ReactNode,
-  /**
-   * Path to redirect if the user is not authenticated
-   *
-   * @deprecated Use AuthProvider fallbackPath prop instead.
-   * Will be removed in the upcoming version
-   * @example
-   * `/login`
-   */
-  fallbackPath?: string
 }
 
 /**
@@ -41,7 +28,7 @@ interface RequireAuthProps {
  *        <Route path={"/"} element={<Home/>}/>
  *        <Route path={"/login"} element={<Login/>}/>
  *        <Route path={"/secure"} element={
- *          <RequireAuth fallbackPath={"/login"}>
+ *          <RequireAuth>
  *            <SecureComponent/>
  *          </RequireAuth>
  *        }/>
@@ -52,34 +39,8 @@ interface RequireAuthProps {
  * ```
  */
 
-function RequireAuth({children, fallbackPath}: RequireAuthProps): React.ReactNode {
-  const config = useReactAuthKitConfig();
-  const isAuthenticated = useIsAuthenticated();
-
-  let fp;
-  if (fallbackPath !== undefined) {
-    fp = fallbackPath;
-  } else if (
-    config.fallbackPath !== undefined
-  ) {
-    fp = config.fallbackPath;
-  } else {
-    throw new AuthKitConfigError(
-      'fallbackPath prop must be present'+
-      ' in AuthProvider or RequireAuth component',
-    );
-  }
-
-
-  if (!isAuthenticated()) {
-    // Redirect them to the /login page but save the current location they
-    // were trying to go to when they were redirected. This allows us to
-    // send them along to that page after they log in, which is a nicer
-    // user experience than dropping them off on the home page.
-
-    return <Navigate to={fp} replace />;
-  }
-
+function RequireAuth({children}: RequireAuthProps): React.ReactNode {
+  useIsAuthenticated();
   return children;
 }
 
